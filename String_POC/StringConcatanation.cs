77cd60540a22303259a69baf6b6bdf3c8b859833
Ragini340 <ragini31bxr@gmail.com>
2024-11-20 22:57:40 +0530

    #CSharp_Practice String Concatanation and String Creation using Constructor

---
+++ String_POC/StringConcatanation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC
+{
+    public class StringConcatanation
+    {
+        public static void Main()
+        {
+            string s1 = "Birds";
+            string s2 = "of";
+            string s3 = "a";
+            string s4 = "feather";
+
+            //Using concatenation operator
+            string str = s1 + s2 + s3 + s4 + "flock";
+
+            Console.WriteLine(str);
+        }
+
+    }
+}
---
+++ String_POC/StringCreationUsingConstructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC
+{
+    public class StringCreationUsingConstructor
+    {
+        public static void Main()
+        {
+            char[] chars = { 'a', 'b', 'c', 'd', 'e' };
+
+            //Creating a string from a character array
+            string str1 = new string(chars);
+            Console.WriteLine(str1);
+
+            //Creating a string that consists of a character repeated 10 times
+            string str2 = new string('a', 10);
+            Console.WriteLine(str2);
+        }
+
+    }
+}
