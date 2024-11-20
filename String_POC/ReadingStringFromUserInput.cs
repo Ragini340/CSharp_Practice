d005cee5b60d3962150dcb522121a9f36fc00944
Ragini340 <ragini31bxr@gmail.com>
2024-11-20 21:29:20 +0530

    #CSharp_Practice Implementation for Reading String from User Input

---
+++ String_POC/ReadingStringFromUserInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC
+{
+    public class ReadingStringFromUserInput
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String: ");
+            string read = Console.ReadLine();
+            Console.WriteLine("User has been entered : " + read);
+        }
+
+    }
+}
