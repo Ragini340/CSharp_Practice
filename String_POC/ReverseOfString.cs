7b34c8593f5246df9a7d427889df3bee1e888d7d
Ragini340 <ragini31bxr@gmail.com>
2024-11-21 08:45:09 +0530

    #CSharp_Practice Reverse of String

---
+++ String_POC/ReverseOfString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC
+{
+    public class ReverseOfString
+    {
+        public static string Reverse(string input)
+        {
+            //Converting string to character array
+            char[] charArray = input.ToCharArray();
+
+            //Declaring an empty string
+            string reversedString = String.Empty;
+
+            //Iterating the each character from right to left 
+            for (int i = charArray.Length - 1; i > -1; i--)
+            {
+                //Appending each character to the reversed string
+                reversedString += charArray[i];
+            }
+
+            //Returning the reversed string
+            return reversedString;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("Best"));
+        }
+
+    }
+}
