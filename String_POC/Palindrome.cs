96200bc4e9ecb9a7b2e58ddf878767a6147ccc1e
Ragini340 <ragini31bxr@gmail.com>
2024-11-21 07:45:52 +0530

    #CSharp_Practice  Checking if the given string is palindrome

---
+++ String_POC/Palindrome.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC
+{
+    public class Palindrome
+    {
+        //Function to check if a string is a palindrome
+        static int IsPalindrome(string s)
+        {
+            int len = s.Length;
+
+            //Iterating over the first half of the string
+            for (int i = 0; i < len / 2; i++)
+            {
+                //If the characters at symmetric positions are not equal
+                if (s[i] != s[len - i - 1])
+                {
+                    return 0;
+                }
+            }
+
+            //If all symmetric characters are equal then it is palindrome
+            return 1;
+        }
+
+        static void Main()
+        {
+            string s = "abba";
+            Console.WriteLine(IsPalindrome(s));
+        }
+
+    }
+}
