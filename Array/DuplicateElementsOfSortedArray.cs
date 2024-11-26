6a6c6ebe1d53243509392f1f03f14bf9d3f21aaa
Ragini340 <ragini31bxr@gmail.com>
2024-11-26 14:17:03 +0530

    #CSharp_Practice Implemented to Find Duplicate Elements In a Sorted Array

---
+++ Array/DuplicateElementsOfSortedArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Array
+{
+    /*Time Complexity : O(log n)
+     Space Complexity: O(1)*/
+    public class DuplicateElementsOfSortedArray
+    {
+        public static int findDuplicateElement(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 4 };
+            int arrIndex = findDuplicateElement(arr, arr.Length);
+            if (arrIndex != -1)
+            {
+                Console.WriteLine("Duplicate element is : " + arr[arrIndex]);
+            }
+        }
+
+    }
+}
