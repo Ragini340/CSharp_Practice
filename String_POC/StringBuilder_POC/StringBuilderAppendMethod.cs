447ea3d2062ee3a3bbef5f3100a0f42dc63e3771
Ragini340 <ragini31bxr@gmail.com>
2024-11-30 18:42:24 +0530

    #CSharp_Practice Implemented Append Method of StringBuilder

---
+++ String_POC/StringBuilder_POC/StringBuilderAppendMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC.StringBuilder_POC
+{
+    public class StringBuilderAppendMethod
+    {
+        public static void Main()
+        {
+            StringBuilder s = new StringBuilder("Hello ", 20);
+            s.Append("abc");
+            //AppendLine() method appends the string with a newline at the end.
+            s.AppendLine("def");
+            s.Append("ghi");
+            Console.WriteLine(s);
+        }
+
+    }
+}
