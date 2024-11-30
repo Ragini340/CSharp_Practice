2ad2f2f1b993d1a35d78ad59bece56d6703a3f26
Ragini340 <ragini31bxr@gmail.com>
2024-11-30 19:02:47 +0530

    #CSharp_Practice Implemented StringBuilder AppendFormat Method

---
+++ String_POC/StringBuilder_POC/StringBuilderAppendFormatMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC.StringBuilder_POC
+{
+    public class StringBuilderAppendFormatMethod
+    {
+        public static void Main()
+        {
+            StringBuilder s = new StringBuilder("The total amount is: ");
+            s.AppendFormat("{0:C} ", 50);
+            Console.WriteLine(s);
+        }
+
+    }
+}
