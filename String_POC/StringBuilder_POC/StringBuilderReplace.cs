b745c40e3ecdd31c5045b0bd18d50b347487f9e7
Ragini340 <ragini31bxr@gmail.com>
2024-11-30 19:39:26 +0530

    #CSharp_Practice Implemented StringBuilder Replace Method

---
+++ String_POC/StringBuilder_POC/StringBuilderReplace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC.StringBuilder_POC
+{
+    public class StringBuilderReplace
+    {
+        public static void Main()
+        {
+            StringBuilder s = new StringBuilder("ABC defg ", 20);
+            //Replace "ABC" with "Pqr xyz" 
+            s.Replace("ABC", "Pqr xyz");
+            Console.WriteLine(s);
+        }
+
+    }
+}
