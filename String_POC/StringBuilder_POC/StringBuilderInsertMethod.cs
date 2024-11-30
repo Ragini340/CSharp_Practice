56fc56e785e029134df1629dd360018a00bb2050
Ragini340 <ragini31bxr@gmail.com>
2024-11-30 19:09:15 +0530

    #CSharp_Practice Implemented StringBuilder Insert Method

---
+++ String_POC/StringBuilder_POC/StringBuilderInsertMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.String_POC.StringBuilder_POC
+{
+    public class StringBuilderInsertMethod
+    {
+        public static void Main()
+        {
+            StringBuilder s = new StringBuilder("Hello ", 20);
+            //"Abc" insert after 6th index 
+            s.Insert(6, "Abc");
+            Console.WriteLine(s);
+        }
+
+    }
+}
