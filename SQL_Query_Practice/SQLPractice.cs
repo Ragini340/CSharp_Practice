3bea90129b21ef1ffe929bc68a1e74b52ba9d8f8
Ragini340 <ragini31bxr@gmail.com>
2024-12-04 20:29:33 +0530

    #CSharp_Practice SQL Challenge

---
+++ SQL_Query_Practice/SQLPractice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.SQL_Query_Practice
+{
+    internal class SQLPractice
+    {
+        /*SELECT e.Position, e.FirstName, e.LastName, s.Position AS ReportsToPosition
+
+       FROM maintable_48570 e
+
+       JOIN maintable_48570 s ON e.ReportsTo = s.Position
+ 
+       WHERE e.ReportsTo IS NOT NULL
+
+       ORDER BY s.Position, e.Position;*/
+    }
+}
