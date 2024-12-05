558691d1f385cd3ec940882de48ec6f3ba0acd14
Ragini340 <ragini31bxr@gmail.com>
2024-12-05 09:04:39 +0530

    #CSharp_Practice Created Sample Hashset

---
+++ HashSet/SampleHashSet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.HashSet
+{
+    public class SampleHashSet
+    {
+        public static void Main(string[] args)
+        {
+            HashSet<int> oddNumbers = new HashSet<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                oddNumbers.Add(2 * i + 1);
+            }
+            foreach (int odds in oddNumbers)
+            {
+                Console.WriteLine(odds);
+            }
+        }
+    }
+}
