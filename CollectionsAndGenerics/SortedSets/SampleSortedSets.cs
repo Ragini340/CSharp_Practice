445d23816690b4cdce4d57dccc318d94b13d973b
Ragini340 <ragini31bxr@gmail.com>
2024-12-06 09:10:57 +0530

    #CSharp_Practice Implemented SortedSets and Remove Method in SortedSet

---
+++ CollectionsAndGenerics/SortedSets/SampleSortedSets.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CollectionsAndGenerics.SortedSets
+{
+    public class SampleSortedSets
+    {
+        public static void Main()
+        {
+            SortedSet<int> sortedSet1 = new SortedSet<int>
+            {
+                { 10 }, { 15 }, { 20 }
+            };
+            Console.WriteLine("Elements in sortedSet1 are:");
+            foreach (int set in sortedSet1)
+            {
+                Console.WriteLine(set);
+            }
+
+            SortedSet<string> sortedSet2 = new SortedSet<string>();
+            sortedSet2.Add("abc");
+            sortedSet2.Add("xyd");
+            sortedSet2.Add("qav");
+            Console.WriteLine("Elements in sortedSet2 are:");
+            foreach(string value in sortedSet2)
+            {
+                Console.WriteLine(value);
+            }
+
+            //Remove method
+            sortedSet1.Remove(15);
+            Console.WriteLine("Remaining elements in sortedSet1 after removing element 15:");
+            foreach (int set in sortedSet1)
+            {
+                Console.WriteLine(set);
+            }
+
+        }
+    }
+} 
