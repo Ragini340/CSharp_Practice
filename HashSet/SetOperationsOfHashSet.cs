04905d8d6fbb7399270ebccdc0a1f0a328b014cf
Ragini340 <ragini31bxr@gmail.com>
2024-12-06 06:46:16 +0530

    #CSharp_Practice Implemented ExceptWith Set Operator of HashsSet

--- HashSet/SetOperationsOfHashSet.cs
+++ HashSet/SetOperationsOfHashSet.cs
@@ -51,6 +51,31 @@ namespace CSharp_Practice.HashSet
             {
                 Console.WriteLine(element);
             }
+
+            //ExceptWith
+            HashSet<string> hashSet1 = new HashSet<string>();
+            hashSet1.Add("A");
+            hashSet1.Add("B");
+            hashSet1.Add("C");
+            hashSet1.Add("D");
+            hashSet1.Add("E");
+            hashSet1.Add("F");
+
+            HashSet<string> hashSet2 = new HashSet<string>();
+            hashSet2.Add("A");
+            hashSet2.Add("Bb");
+            hashSet2.Add("Cc");
+            hashSet2.Add("Dd");
+            hashSet2.Add("Ee");
+            hashSet2.Add("F");
+
+            hashSet1.ExceptWith(hashSet2);
+            Console.WriteLine("Elements after using ExceptWith Operator on hashSet1 and hashSet2:");
+            foreach (var element in hashSet1)
+            {
+                Console.WriteLine(element);
+            }
+
         }
 
     }
