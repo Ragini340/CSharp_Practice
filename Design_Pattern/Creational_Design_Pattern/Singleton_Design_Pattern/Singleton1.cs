bf39c2662141230988921adc3beecee8f1232187
Ragini340 <ragini31bxr@gmail.com>
2024-12-07 11:15:24 +0530

    #CShrap_Practice Implemented Singleton Class With Lazy Initialization

---
+++ Design_Pattern/Creational_Design_Pattern/Singleton_Design_Pattern/Singleton1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Design_Pattern.Creational_Design_Pattern.Singleton_Design_Pattern
+{
+    public class Singleton1
+    {
+        private static Singleton1 instance;
+        private Singleton1()
+        {
+        }
+
+        private static Singleton1 GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton1();
+            }
+            return instance;
+        }
+
+        public static void Main(string[] args)
+        {
+            Singleton1 obj1 = Singleton1.GetInstance();
+            Console.WriteLine(obj1.GetHashCode());
+            //obj1 & obj2 both will have same HashCode value
+            Singleton1 obj2 = Singleton1.GetInstance();
+            Console.WriteLine(obj2.GetHashCode());
+        }
+
+    }
+}
