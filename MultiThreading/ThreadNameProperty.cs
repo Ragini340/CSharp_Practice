41a627ff3509b2d220c205794660c5df6304b62a
Ragini340 <ragini31bxr@gmail.com>
2024-12-08 13:27:36 +0530

    #CSharp_Practice Implemented Property Name in Thread

---
+++ MultiThreading/ThreadNameProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharp_Practice.MultiThreading
+{
+    public class ThreadNameProperty
+    {
+        public static void Main()
+        {
+            Thread thread = Thread.CurrentThread;
+            thread.Name = "Main thread";
+            //Console.WriteLine("Current executing thread name is: " + thread.Name);
+            Console.WriteLine("Current executing thread name is: " + Thread.CurrentThread.Name);
+        }
+
+    }
+}
