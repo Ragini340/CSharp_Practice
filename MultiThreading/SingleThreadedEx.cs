072be6e2d73fdf994802f37653a386b38d867be4
Ragini340 <ragini31bxr@gmail.com>
2024-12-08 13:41:55 +0530

    #CSharp_Practice Implemented Single Thread Example

---
+++ MultiThreading/SingleThreadedEx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.MultiThreading
+{
+    public class SingleThreadedEx
+    {
+        public static void Method1()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("This is method1:" + i);
+            }
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("This is method2:" + i);
+            }
+        }
+
+        public static void Method3()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("This is method3:" + i);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Method1();
+            Console.WriteLine();
+            Method2();
+            Console.WriteLine();
+            Method3();
+            Console.WriteLine();
+        }
+
+    }
+}
