0d4c8b1b7c0df56b2af059145d269a4bbe16edb8
Ragini340 <ragini31bxr@gmail.com>
2024-12-08 10:53:12 +0530

    CSharp_Practice Implemented Thread

---
+++ MultiThreading/ThreadCreation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.MultiThreading
+{
+    public class ThreadCreation
+    {
+        public static void Main()
+        {
+            Thread thread = new Thread(Worker);
+            thread.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread is doing some work");
+                Thread.Sleep(100);
+            }
+            thread.Join();
+            Console.WriteLine("Work done");
+
+        }
+
+        public static void Worker()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Worker thread is doing some work");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
