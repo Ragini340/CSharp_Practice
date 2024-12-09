c61af4e438227fc33a6a37ca2d19aa9a9df61c91
Ragini340 <ragini31bxr@gmail.com>
2024-12-09 20:41:16 +0530

    #CSharp_Practice Implemented Destructor

---
+++ Destructors/DestructorDriverClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Destructors
+{
+    public class DestructorDriverClass
+    {
+        public static void Main()
+        {
+            DestructorEx destructorEx = new DestructorEx();
+            destructorEx.SetValues(2, 3);
+            destructorEx.DisplayValue();
+        }
+    }
+}
---
+++ Destructors/DestructorEx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Destructors
+{
+    public class DestructorEx
+    {
+        int real;
+        int imaginary;
+
+        public DestructorEx()
+        {
+            real = 0;
+            imaginary = 0;
+        }
+
+        public void SetValues(int r, int i)
+        {
+            real = r;
+            imaginary = i;
+        }
+
+        public void DisplayValue()
+        {
+            Console.WriteLine("Real: " + real);
+            Console.WriteLine("Imaginary: " + imaginary);
+        }
+
+        /*
+         * Destructor is not working in VS, but in online compiler it is working
+         * https://www.sololearn.com/en/compiler-playground/ciHFz1vNThhY
+        */
+        ~DestructorEx()
+        {
+            Console.WriteLine("Destructor was called");
+        }
+
+    }
+}
