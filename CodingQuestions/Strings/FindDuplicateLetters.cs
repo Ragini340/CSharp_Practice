d509478d79e29c0bd60f4f32377e5eecf4fc90bb
Ragini340 <ragini31bxr@gmail.com>
2024-12-09 11:52:02 +0530

    #CSharp_Practice Implementation for Finding Duplicate Letter of a String

---
+++ CodingQuestions/Strings/FindDuplicateLetters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CodingQuestions.Strings
+{
+    /*Write a program to find duplicate letter using Dictionary*/
+    public class FindDuplicateLetters
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = "GeeksforGeeks";
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (char c in str1)
+            {
+                if (dictionary.ContainsKey(c))
+                {
+                    dictionary[c]++;
+                }
+                else
+                {
+                    dictionary[c] = 1;
+                }
+            }
+
+            Console.WriteLine("Duplicate characters and their counts are:");
+            foreach (KeyValuePair<char, int> kvps in dictionary)
+            {
+                if (kvps.Value > 1)
+                {
+                    Console.WriteLine("{0} {1}", kvps.Key, kvps.Value);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ CodingQuestions/Strings/FindDuplicateLettersUsingArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CodingQuestions.Strings
+{
+    /*Write a program to find duplicate letter using array*/
+    public class FindDuplicateLettersUsingArray
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = "RaginiR";
+           /* char[] characterArray = new char[str1.Length];
+            for (int i = 0; i < str1.Length; i++)
+            {
+                characterArray[i] = str1[i];
+            }
+            Console.WriteLine(characterArray);*/
+
+            //Using ToCharArray() Method
+            char[] characterArray = str1.ToCharArray();
+            Console.WriteLine(characterArray);
+   
+            int count;
+            Console.WriteLine("Duplicates are: ");
+            for (int i = 0; i < characterArray.Length; i++)
+            {
+                count = 1;
+                for (int j = i + 1; j < characterArray.Length; j++)
+                {
+                    if (characterArray[i] == characterArray[j])
+                    {
+                        count++;
+                        characterArray[j] = '1';
+                    }
+                }
+
+                if (count > 1 && characterArray[i] != '1')
+                {
+                    Console.WriteLine(characterArray[i]);
+                }
+            }
+
+        }
+    }
+}
