bf29e2ac8d7e74ec4db498631ec43b1825006743
Ragini340 <ragini31bxr@gmail.com>
2024-12-13 23:09:34 +0530

    #CSharp_Practice Implemented Multiple Inheritance with Interfaces

---
+++ Interface/Diamond_Problem/B.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Interface.Diamond_Problem
+{
+    public interface B
+    {
+        public void Check();
+        public void Print();
+    }
+}
---
+++ Interface/Diamond_Problem/C.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Interface.Diamond_Problem
+{
+    public class C : A, B
+    {
+        public void Check()
+        {
+            Console.WriteLine("Check method");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Print method");
+        }
+    }
+
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            C obj = new C();
+            obj.Check();
+            obj.Print();
+        }
+    }
+
+}
