af720c6ab2612000fdff9c22aecddca310d19661
Ragini340 <ragini31bxr@gmail.com>
2024-12-13 19:36:13 +0530

    #CSharp_Practice Implemented code to reverse the whole sentence

---
+++ CodingQuestions/Strings/ReverseWholeSentence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CodingQuestions.Strings
+{
+    //This program will reverse the whole sentence
+    public class ReverseWholeSentence
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence: ");
+            string str1 = Console.ReadLine();
+
+            string[] arrStr = str1.Split(" ");
+            string reverseWord1 = "";
+
+            for (int i = arrStr.Length - 1; i >= 0; i--)
+            {
+                string reverseWord = "";
+                reverseWord = reverseWord + arrStr[i];
+                reverseWord1 = reverseWord1 + reverseWord + " ";
+            }
+            reverseWord1 = reverseWord1.Trim();
+
+            Console.WriteLine("Revered sentence is: " + reverseWord1);
+            Console.WriteLine("Length of the revered sentence is: " + reverseWord1.Length);
+        }
+
+    }
+}
