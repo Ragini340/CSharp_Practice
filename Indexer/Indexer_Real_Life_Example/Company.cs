aca55487ec891e68f942387bd70aa62718115b73
Ragini340 <ragini31bxr@gmail.com>
2024-12-15 21:00:51 +0530

    #CSharp_Practice Implemented Indexer

---
+++ Indexer/Indexer_Real_Life_Example/Company.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Indexer.Indexer_Real_Life_Example
+{
+    public class Company
+    {
+        private List<Employee> employees = new List<Employee>();
+        public Company()
+        {
+            employees.Add(new Employee { EmployeeId = 1, Name = "A", Gender = "F", Salary = 100 });
+            employees.Add(new Employee { EmployeeId = 2, Name = "B", Gender = "M", Salary = 1000 });
+            employees.Add(new Employee { EmployeeId = 3, Name = "C", Gender = "M", Salary = 10000 });
+            employees.Add(new Employee { EmployeeId = 4, Name = "D", Gender = "M", Salary = 100000 });
+            employees.Add(new Employee { EmployeeId = 5, Name = "E", Gender = "F", Salary = 1000000 });
+        }
+
+        public string this[int employeeId]
+        {
+            get
+            {
+                foreach (Employee e in employees)
+                {
+                    if (e.EmployeeId == employeeId)
+                    {
+                        return e.Name;
+                    }
+                }
+                return null;
+            }
+            set
+            {
+                foreach (Employee e in employees)
+                {
+                    if (e.EmployeeId == employeeId)
+                    {
+                        e.Name = value;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Indexer/Indexer_Real_Life_Example/Driver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Indexer.Indexer_Real_Life_Example
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            Company company = new Company();
+            Console.WriteLine("Name of Employee with EmployeeId 1: " + company[1]);
+            Console.WriteLine("Name of Employee with EmployeeId 2: " + company[2]);
+            Console.WriteLine("Name of Employee with EmployeeId 3: " + company[3]);
+            Console.WriteLine("Name of Employee with EmployeeId 4: " + company[4]);
+            Console.WriteLine("Name of Employee with EmployeeId 5: " + company[5]);
+            Console.WriteLine();
+
+            company[1] = "AA";
+            company[2] = "BB";
+            company[3] = "CC";
+
+            Console.WriteLine("Updated Name of the Employee with EmployeeId 1: " + company[1]);
+            Console.WriteLine("Updated Name of the Employee with EmployeeId 2: " + company[2]);
+            Console.WriteLine("Updated Name of the Employee with EmployeeId 3: " + company[3]);
+        }
+    }
+}
