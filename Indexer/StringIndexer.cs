e7c510ce67c11e3ea3dcd8e01164860c6b13bebe
Ragini340 <ragini31bxr@gmail.com>
2024-12-15 09:12:05 +0530

    #CSharp_Practice Implemented String Indexer

---
+++ Indexer/StringIndexer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Indexer
+{
+    public class StringIndexer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public double Salary { get; set; }
+        public string Location { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+
+        public StringIndexer(int Id, string Name, string Job, int Salary, string Location,
+                        string Department, string Gender)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Job = Job;
+            this.Salary = Salary;
+            this.Location = Location;
+            this.Department = Department;
+            this.Gender = Gender;
+        }
+
+        public object this[string index]
+        {
+            get
+            {
+                if (index == "Id")
+                {
+                    return Id;
+                }
+                else if (index == "Name")
+                {
+                    return Name;
+                }
+                else if (index == "Job")
+                {
+                    return Job;
+                }
+                else if (index == "Salary")
+                {
+                    return Salary;
+                }
+                else if (index == "Location")
+                {
+                    return Location;
+                }
+                else if (index == "Department")
+                {
+                    return Department;
+                }
+                else if (index == "Gender")
+                {
+                    return Gender;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            set
+            {
+                if (index == "Id")
+                {
+                    Id = Convert.ToInt32(value);
+                }
+                else if (index == "Name")
+                {
+                    Name = value.ToString();
+                }
+                else if (index == "Job")
+                {
+                    Job = value.ToString();
+                }
+                else if (index == "Salary")
+                {
+                    Salary = Convert.ToDouble(value);
+                }
+                else if (index == "Location")
+                {
+                    Location = value.ToString();
+                }
+                else if (index == "Department")
+                {
+                    Department = value.ToString();
+                }
+                else if (index == "Gender")
+                {
+                    Gender = value.ToString();
+                }
+            }
+
+        }
+    }
+}
---
+++ Indexer/StringIndexerDriver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Indexer
+{
+    public class StringIndexerDriver
+    {
+        public static void Main(string[] args)
+        {
+            StringIndexer stringIndexer = new StringIndexer(100, "PQR", "SDE", 10000000, "Mumbai", "IT", "Male");
+            Console.WriteLine("Id: " + stringIndexer["Id"]);
+            Console.WriteLine("Name: " + stringIndexer["Name"]);
+            Console.WriteLine("Job: " + stringIndexer["Job"]);
+            Console.WriteLine("Salary: " + stringIndexer["Salary"]);
+            Console.WriteLine("Location: " + stringIndexer["Location"]);
+            Console.WriteLine("Department: " + stringIndexer["Department"]);
+            Console.WriteLine("Gender: " + stringIndexer["Gender"]);
+            Console.WriteLine();
+
+            stringIndexer["Name"] = "Xyz";
+            stringIndexer["Salary"] = 5000;
+            stringIndexer["Location"] = "BLR";
+
+            Console.WriteLine("Id: " + stringIndexer["Id"]);
+            Console.WriteLine("Name: " + stringIndexer["Name"]);
+            Console.WriteLine("Job: " + stringIndexer["Job"]);
+            Console.WriteLine("Salary: " + stringIndexer["Salary"]);
+            Console.WriteLine("Location: " + stringIndexer["Location"]);
+            Console.WriteLine("Department: " + stringIndexer["Department"]);
+            Console.WriteLine("Gender: " + stringIndexer["Gender"]);
+        }
+    }
+}
