63f252c94a61c579946b7b882f911c496e1ccdd7
Ragini340 <ragini31bxr@gmail.com>
2024-12-17 22:30:42 +0530

    #CSharp_Practice Implemented  FibonacciSeries Series Upto 5

---
+++ CodingQuestions/FibonacciSeries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CodingQuestions
+{
+    public class FibonacciSeries
+    {
+        public static int Fibonacci(int num)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < num; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        static void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+    }
+}
