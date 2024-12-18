072858d090b2fd1552670fb00fa8ec2c0e514b3e
Ragini340 <ragini31bxr@gmail.com>
2024-12-18 10:47:15 +0530

    #CSharp_Practice Modified Second Smallest Element in a List

--- CodingQuestions/Lists/SecondLargestElementInList.cs
+++ CodingQuestions/Lists/SecondLargestElementInList.cs
@@ -11,7 +11,7 @@ namespace CSharp_Practice.CodingQuestions.Lists
         //Program to find second largest number in a list
         public static void Main(string[] args)
         {
-            List<int> lists = new List<int> { 5, 3, 2, 6, 7 };
+            List<int> lists = new List<int> { 5, 3, 2, 6, 7, 7, 7 };
             Console.WriteLine("Elements of list are: ");
             foreach (int list in lists)
             {
@@ -28,16 +28,18 @@ namespace CSharp_Practice.CodingQuestions.Lists
             }
             Console.WriteLine();
 
-            lists.Reverse();
-            Console.WriteLine("Sorted list's elements in descending order are: ");
-            for (int i = 0; i < lists.Count; i++)
-            {
-                int num = lists[i];
-                Console.WriteLine(num);
-            }
+            Console.WriteLine("Size of the list is: " + lists.Count);
             Console.WriteLine();
 
-            Console.WriteLine("Second largest number is: " + lists[1]);
+            for (int i = lists.Count - 2; i >= 0; i--)
+            {
+                if (lists[i] != lists[lists.Count - 1])
+                {
+                    int num = lists[i];
+                    Console.WriteLine("Second largest number is: " + num);
+                    break;
+                }
+            } 
         }
 
     }
