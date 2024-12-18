9b33b81882b56f4d8c6c6daa0eeb61f5bdfe7445
Ragini340 <ragini31bxr@gmail.com>
2024-12-18 10:48:45 +0530

    #CSharp_Practice Implementation for Finding Second Smallest Number in a List

---
+++ CodingQuestions/Lists/SecondSmallestElementInList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CodingQuestions.Lists
+{
+    public class SecondSmallestElementInList
+    {
+        //Program to find second smallest number in a list
+        public static void Main(string[] args)
+        {
+            List<int> lists = new List<int> { 5, 3, 2, 2, 6, 7, 7, 7 };
+            Console.WriteLine("Elements of list are: ");
+            foreach (int list in lists)
+            {
+                Console.WriteLine(list);
+            }
+            Console.WriteLine();
+
+            lists.Sort();
+            Console.WriteLine("Sorted list's elements in ascending order are: ");
+            for (int i = 0; i < lists.Count; i++)
+            {
+                int num = lists[i];
+                Console.WriteLine(num);
+            }
+            Console.WriteLine();
+
+            lists.Reverse();
+            Console.WriteLine("Sorted list's elements in descending order are: ");
+            for (int i = 0; i < lists.Count; i++)
+            {
+                int num = lists[i];
+                Console.WriteLine(num);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Size of the list is: " + lists.Count);
+            Console.WriteLine();
+
+            for (int i = lists.Count - 2; i >= 0; i--)
+            {
+                if (lists[i] != lists[lists.Count - 1])
+                {
+                    int num = lists[i];
+                    Console.WriteLine("Second smallest number is: " + num);
+                    break;
+                }
+            }
+        }
+
+    }
+}
