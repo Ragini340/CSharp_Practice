e964ec8a7f2afaec698c104fb877a38c2c8e0900
Ragini340 <ragini31bxr@gmail.com>
2024-12-19 10:27:36 +0530

    #CSharp_Practice Implementation for Finding LCM of Two Numbers

---
+++ CodingQuestions/LCMOfTwoNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CodingQuestions
+{
+    public class LCMOfTwoNumbers
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2, max, lcm;
+            Console.WriteLine("Enter two numbers:");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            max = (num1 > num2) ? num1 : num2;
+            lcm = max;
+
+            while ((lcm % num1 != 0) || (lcm % num2 != 0))
+            {
+                lcm = lcm + max;
+            }
+            Console.WriteLine("LCM of two numbers are: " +lcm);
+        }
+    }
+
+}
