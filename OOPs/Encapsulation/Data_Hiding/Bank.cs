a0db630d7e70eefb3d9c7baed7d38f3afb9668e4
Ragini340 <ragini31bxr@gmail.com>
2024-12-20 18:29:57 +0530

    #CSharp_Practice Implemented Encapsulations

---
+++ OOPs/Encapsulation/Data_Hiding/Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.OOPs.Encapsulation.Data_Hiding
+{
+    public class Bank
+    {
+        private double balance;
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void SetBalance(double balance)
+        {
+            this.balance = balance;
+        }
+    }
+
+    public class Driver
+    {
+        public static void Main()
+        {
+            Bank bank = new Bank();
+            bank.SetBalance(100);
+            Console.WriteLine("Balance is: " + bank.GetBalance());
+        }
+    }
+}
---
+++ OOPs/Encapsulation/Data_Hiding/BankDetailsUsingValidation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.OOPs.Encapsulation.Data_Hiding
+{
+    public class BankDetailsUsingValidation
+    {
+        private int Amount;
+        public int GetAmount()
+        {
+            return Amount;
+        }
+
+        public void SetAmount(int Amount)
+        {
+            if (Amount > 0)
+            {
+                this.Amount = Amount;
+            }
+            else
+            {
+                throw new Exception("Please enter a positive value");
+            }
+        }
+    }
+
+    class BankDetailsUsingValidationDriver
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                BankDetailsUsingValidation bankDetailsUsingValidation = new BankDetailsUsingValidation();
+                bankDetailsUsingValidation.SetAmount(100);
+                Console.WriteLine(bankDetailsUsingValidation.GetAmount());
+                bankDetailsUsingValidation.SetAmount(-100);
+                Console.WriteLine(bankDetailsUsingValidation.GetAmount());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
