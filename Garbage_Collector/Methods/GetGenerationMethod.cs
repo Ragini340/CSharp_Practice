ad64a03e098f0b9f1379348e1ca36cb22f0185e0
Ragini340 <ragini31bxr@gmail.com>
2024-12-20 18:09:33 +0530

    #CSharp_Practice Implemented Garbage Collector Methods and Property

---
+++ Garbage_Collector/Methods/GetGenerationMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Garbage_Collector.Methods
+{
+    //This method returns the generation number of the target object.
+    //It requires a single parameter that is the target object for which the generation number is required.
+    //In the below code, GetGeneration() method will return the generation number of the target object.
+    //Object of the class will be passed as a input parameter for the GetGeneration() method.
+    //When we will execute the calss it will show the output as 0.
+    public class GetGenerationMethod
+    {
+        public static void Main(string[] args)
+        {
+            GetGenerationMethod getGenerationMethod = new GetGenerationMethod();
+            Console.WriteLine("Total number of generations for the created getGenerationMethod object is: " + GC.GetGeneration(getGenerationMethod));
+            GC.Collect(0);
+            Console.WriteLine("Garbage collection in generation 0 is: " + GC.CollectionCount(0));
+        }
+    }
+}
---
+++ Garbage_Collector/Properties/MaxGenerationProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Garbage_Collector.Properties
+{
+    //This property in the GC class returns the total number of generations
+    public class MaxGenerationProperty
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Garbage collection maximum generations are: " + GC.MaxGeneration);
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+        }
+    }
+}
