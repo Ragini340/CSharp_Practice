9780ef2d7bd5201eaa0a559eae8b1dc6b84c153d
Ragini340 <ragini31bxr@gmail.com>
2024-12-21 20:19:56 +0530

    #CSharp_Practice Implemented Single Inheritance

--- OOPs/Constructor/DefaultConstructor.cs
+++ OOPs/Constructor/DefaultConstructor.cs
@@ -11,10 +11,12 @@ namespace CSharp_Practice.OOPs.Constructor
         public int num = 1;
         public int age = 20;
 
-        public DefaultConstructor()
-        {
+        public DefaultConstructor(){
             Console.WriteLine("Default constructor called");
+            
         }
+
+     
 
     }
 }
---
+++ OOPs/Inheritance/Single_Inheritance/B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.OOPs.Inheritance.Single_Inheritance
+{
+    public class B : A
+    {
+        //If I am overriding k as below then it will print B class k's value not of A's class and it is hiding inherited member k of class A:
+        //public int k = 20;
+        public void Method3()
+        {
+            Console.WriteLine("Method3 of class B");
+        }
+
+        public static void Main()
+        {
+            B b = new B();
+            b.Method1();
+            b.Method2();
+            b.Method3();
+            Console.WriteLine("Value of k: " + b.k);
+        }
+    }
+}
