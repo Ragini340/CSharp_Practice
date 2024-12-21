6d48bdbca62a8bab0e8c5dfb5ebb33d13c30e258
Ragini340 <ragini31bxr@gmail.com>
2024-12-21 21:21:44 +0530

    #CSharp_Practice Constructor POC

---
+++ OOPs/Constructor/ConstructorPOC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.OOPs.Constructor
+{
+    public class ConstructorPOC
+    {
+        //If we want to write only single line (Single statement) in a constructor then we can use arrow function as:
+        //public ConstructorPOC() => Console.WriteLine("Deafult Constructor"); 
+        int i;
+        //If we are defining default constructor explicitely then the constructor is known as explicit constructor. Compiler implicitely defines only the default constructor, which is by default public and called implicit constructor.
+        public ConstructorPOC()
+        {   //If we don't write any constructor in a class then by default when we create object of the class then the compiler will create default construtor.
+            Console.WriteLine("Deafult constructor");
+        }
+        public ConstructorPOC(String str)
+        {
+            //If we create object of class using parametrized constructor then we must need to define parametrized constructor in class with same type of argument.
+            Console.WriteLine("I am parametrized constructor with one parameter of type string");
+            return;
+        }
+
+        public static void Main()
+        {
+            //ConstructorPOC poc = new ConstructorPOC(1);  //Technical error will be thrown because there is no parametrized constructor with argument type integer.
+            ConstructorPOC poc = new ConstructorPOC("Ragini");
+            Console.WriteLine(poc.i); //Default value of i will be zero. Default value for string type will be null and for bool type it will be false.
+        }
+    }
+}
