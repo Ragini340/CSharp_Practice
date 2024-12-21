180069744b27ab8a4a77d6bb6ea6c83040bcec5f
Ragini340 <ragini31bxr@gmail.com>
2024-12-22 00:43:10 +0530

    #CSharp_Practice Implemented Sum of Natural Numbers Using Recursion

---
+++ Recursion/SumOfNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Recursion
+{
+    public class SumOfNumbers
+    {
+        int n = 1;
+        public int FindSum(int n) 
+        {
+            if(n == 1)
+            {
+                return 1;
+            }
+          
+            return FindSum(n - 1) + n;
+        }
+    }
+
+    public class SumOfNumbersDriver
+    {
+        public static void Main()
+        {
+            int n = 10;
+            SumOfNumbers sumOfNumbers = new SumOfNumbers();
+            int sum = sumOfNumbers.FindSum(n);
+            Console.WriteLine(sum);
+        }
+    }
+
+}
