d6a3a7ad9e90935187a334b10647c7186ea14dd3
Ragini340 <ragini31bxr@gmail.com>
2024-12-21 21:01:04 +0530

    #CSharp_Practice Implemented Exception Under Constructor

---
+++ OOPs/Constructor/ThrowExceptionWithConstructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.OOPs.Constructor
+{
+    //We can throw an exception from the constructor as:
+    public class ThrowExceptionWithConstructor
+    {
+        int result;
+        public ThrowExceptionWithConstructor()
+        {
+            int a = 0, b = 1;
+            if (a == 0)
+            {
+                throw new Exception("Division by zero exception.Ragini");
+            }
+            result = b / a;
+        }
+        public static void Main(string[] args)
+        {
+            ThrowExceptionWithConstructor throwExceptionWithConstructor = new ThrowExceptionWithConstructor();
+            Console.WriteLine(throwExceptionWithConstructor.result);
+        }
+    }
+}
