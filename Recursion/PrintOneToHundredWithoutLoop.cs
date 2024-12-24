23fb6b611becae40b0d3c2692e577030ebeb02f7
Ragini340 <ragini31bxr@gmail.com>
2024-12-24 12:59:44 +0530

    #CSharp_Practice Printed 1 to 100 Without Using any Loop

---
+++ Recursion/PrintOneToHundredWithoutLoop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Recursion
+{
+    public class PrintOneToHundredWithoutLoop
+    {
+        public static void PrintNumbers(int num)
+        {
+            if (num <= 0)
+            {
+                return;
+            }
+
+            PrintNumbers(num - 1);
+            Console.Write(num + " ");
+        }
+
+        public static void Main()
+        {
+            int num = 100;
+            PrintNumbers(num);
+        }
+    }
+}
