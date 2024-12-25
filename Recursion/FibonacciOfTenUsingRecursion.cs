84582dd1a9f54cf517d137ef82f65b868f881203
Ragini340 <ragini31bxr@gmail.com>
2024-12-25 22:09:56 +0530

    #CSharp_Practice Implemented Fibonacci of Ten & Fibonacci Series from 0 to 10 Using Recursion

---
+++ Recursion/FibonacciOfTenUsingRecursion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Recursion
+{
+    public class FibonacciOfTenUsingRecursion
+    {
+        public static int FindFibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            return FindFibonacci(n - 1) + FindFibonacci(n - 2);
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = 10;
+            int fibonacci = FindFibonacci(n);
+            Console.WriteLine(fibonacci);
+        }
+    }
+}
---
+++ Recursion/FibonacciSeriesFromZeroToTen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Recursion
+{
+    public class FibonacciSeriesFromZeroToTen
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static void Main(string[] args)
+        {
+            int n = 10;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+        }
+
+    }
+}
