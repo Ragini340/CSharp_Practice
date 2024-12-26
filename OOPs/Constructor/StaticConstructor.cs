b46155ee8a62219fa879a5a4563e91a777cd6f53
Ragini340 <ragini31bxr@gmail.com>
2024-12-26 20:20:20 +0530

    #CSharp_Practice Updated Static Constructor

--- OOPs/Constructor/StaticConstructor.cs
+++ OOPs/Constructor/StaticConstructor.cs
@@ -8,12 +8,17 @@ namespace CSharp_Practice.OOPs.Constructor
 {
     public class StaticConstructor
     {
+        int i;
+        static int j;
         //Static constructor
         //The static constructors are never called explicitly, they are always called implicitly.
         static StaticConstructor()
         {
             // The following statement produces the first line of output, and the line occurs only once.
             Console.WriteLine("Static Constructor");
+            //i = 100; //We cannot initialize non-static data members in Static constructor
+            //Compile time error:- An object reference is required for the non-static field, method, or property 'member'
+            j = 100;
         }
 
         //Default constructor
