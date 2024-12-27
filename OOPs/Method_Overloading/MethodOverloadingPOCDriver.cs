b52f6ad344143f3f9c75dbff5bd29452d333db1b
Ragini340 <ragini31bxr@gmail.com>
2024-12-27 07:05:34 +0530

    #CSharp_Practice Implemented Method Overloading

---
+++ OOPs/Method_Overloading/MethodOverloadingPOCDriver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.OOPs.Method_Overloading
+{
+    public class MethodOverloadingPOCDriver
+    {
+        public static void Main(string[] args)
+        {
+            MethodOverloadingPOCs methodOverloadingPOCs = new MethodOverloadingPOCs();
+
+            int sum1 = methodOverloadingPOCs.Add(1,2);
+            Console.WriteLine("Sum of the two integer value: " + sum1);
+            int sum2 = methodOverloadingPOCs.Add(1,2,3);
+            Console.WriteLine("Sum of the three integer value: " + sum2);
+
+            int sum3 = methodOverloadingPOCs.Add(1);
+            Console.WriteLine("sum is: " + sum3);
+            double sum4 = methodOverloadingPOCs.Add(1.0);
+            Console.WriteLine("sum is: " + sum4);
+
+            methodOverloadingPOCs.Identity("Gabby", 1);
+            methodOverloadingPOCs.Identity(2, "Bree");
+        }
+    }
+}
---
+++ OOPs/Method_Overloading/MethodOverloadingPOCs.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.OOPs.Method_Overloading
+{
+    public class MethodOverloadingPOCs
+    {
+        //Program to demonstrate the function overloading by changing the Number of parameters
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        public int Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            return sum;
+        }
+
+        //Program to demonstrate the function overloading by changing the Data types of the parameters 
+        public int Add(int a)
+        {
+            int sum = a;
+            return sum;
+        }
+        public double Add(double a)
+        {
+            double sum = a;
+            return sum;
+        }
+
+        //Program to demonstrate the function overloading by changing the Order of the parameters
+        public void Identity(String name, int id)
+        {
+
+            Console.WriteLine("Name1 : " + name + ", " + "Id1 : " + id);
+        }
+        public void Identity(int id, String name)
+        {
+
+            Console.WriteLine("Name2 : " + name + ", " + "Id2 : " + id);
+        }
+
+        //Program to show error when method signature is the same and the return type is different.
+        public int Add(int a, int b, int c, int d)
+        {
+            int sum = a + b + c + d;
+            return sum;
+        }
+        /*Below Add method will throw error as:-
+        Type already defines a member called 'Add' with the same parameter types
+        public double Add(int a, int b, int c, int d)
+        {
+            double sum = a + b + c + d;
+            return sum;
+        }
+        What happens when method signature is same and the return type is different?
+        =>The compiler will give error as the return value alone is not sufficient for the compiler to figure out which function 
+        it has to call. Only if both methods have different parameter types (so, they have the different signature), then Method
+        overloading is possible. Hence, complile time error will be thrown.
+        */
+    }
+}
