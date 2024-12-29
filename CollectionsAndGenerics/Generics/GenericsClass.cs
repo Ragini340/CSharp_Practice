31ef875444883edbdcbef3d780680bb94cee6d12
Ragini340 <ragini31bxr@gmail.com>
2024-12-29 17:21:43 +0530

    #CSharp_Practice Implemented Generic Class, Field, Constructor, Method, and Property

---
+++ CollectionsAndGenerics/Generics/GenericsClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CollectionsAndGenerics.Generics
+{
+    public class GenericsClass<T>
+    {
+        private T GenericMemberVariable;
+
+        public GenericsClass(T value)
+        {
+            GenericMemberVariable = value;
+        }
+
+        public T GenericMethod(T GenericParameter)
+        {
+            Console.WriteLine($"Parameter type: {typeof(T).ToString()}, Value: {GenericParameter}");
+            Console.WriteLine($"Return type: {typeof(T).ToString()}, Value: {GenericMemberVariable}");
+            return GenericMemberVariable;
+        }
+
+        public T GenericProperty { get; set; }
+    }
+}
---
+++ CollectionsAndGenerics/Generics/GenericsClassDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CollectionsAndGenerics.Generics
+{
+    public class GenericsClassDriver
+    {
+        public static void Main(string[] args)
+        {
+            GenericsClass<int> genericsClass = new GenericsClass<int>(100); //Generic constructor
+            int val = genericsClass.GenericMethod(200); //Generic method
+            Console.WriteLine(val); //Generic member variable
+            genericsClass.GenericProperty = 50; //Generic property
+            Console.WriteLine("GenericProperty is: " + genericsClass.GenericProperty); 
+        }
+    }
+}
