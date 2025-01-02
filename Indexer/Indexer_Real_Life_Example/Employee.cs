839c70e3e735e67b108373fd0ddd9951aee45951
Ragini340 <ragini31bxr@gmail.com>
2025-01-02 10:48:53 +0530

    #CSharp_Practice Overriden ToString Method into Employee Model Class

--- Indexer/Indexer_Real_Life_Example/Employee.cs
+++ Indexer/Indexer_Real_Life_Example/Employee.cs
@@ -12,5 +12,10 @@ namespace CSharp_Practice.Indexer.Indexer_Real_Life_Example
         public string Name { get; set; }
         public string Gender { get; set; }
         public double Salary { get; set; }
+
+        public override string ToString()
+        {
+            return "EmployeeId: " + this.EmployeeId + " Name: " + this.Name + " Gender: " + this.Gender + " Salary: " + this.Salary;
+        }
     }
 }
