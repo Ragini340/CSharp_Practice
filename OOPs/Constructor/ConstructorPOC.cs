edd1146ea13c1718260b7adc6c8e61b31a992dd1
Ragini340 <ragini31bxr@gmail.com>
2025-01-02 10:46:35 +0530

    #CSharp_Practice Implementation for Calling One Constructor into Another Constructor

--- OOPs/Constructor/ConstructorPOC.cs
+++ OOPs/Constructor/ConstructorPOC.cs
@@ -12,23 +12,27 @@ namespace CSharp_Practice.OOPs.Constructor
         //public ConstructorPOC() => Console.WriteLine("Deafult Constructor"); 
         int i;
         //If we are defining default constructor explicitely then the constructor is known as explicit constructor. Compiler implicitely defines only the default constructor, which is by default public and called implicit constructor.
-        public ConstructorPOC()
+        public ConstructorPOC() : this("Rag") //Calling other parametrized constructor with one parameter
         {
             //If we don't write any constructor in a class then by default when we create object of the class then the compiler will create default constructor.
             Console.WriteLine("Deafult constructor");
         }
-        public ConstructorPOC(String str)
+        public ConstructorPOC(String str) :this(1, str)
         {
             //If we create object of class using parametrized constructor then we must need to define parametrized constructor in class with same type of argument.
             Console.WriteLine("I am parametrized constructor with one parameter of type string");
             return;
         }
-
+        public ConstructorPOC(int empId, string str)
+        {
+            Console.WriteLine("I am parametrized constructor with 2 parameters" + empId + " " + str);
+        }
         public static void Main()
         {
             //ConstructorPOC poc = new ConstructorPOC(1);  //Technical error will be thrown because there is no parametrized constructor with argument type integer.
-            ConstructorPOC poc = new ConstructorPOC("Ragini");
-            Console.WriteLine(poc.i); //Default value of i will be zero. Default value for string type will be null and for bool type it will be false.
+            //ConstructorPOC poc = new ConstructorPOC("Ragini");
+            ConstructorPOC constructorPOC = new ConstructorPOC();
+            // Console.WriteLine(poc.i); //Default value of i will be zero. Default value for string type will be null and for bool type it will be false.
         }
     }
 }
