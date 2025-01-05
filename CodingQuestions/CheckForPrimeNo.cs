c02ea923918a0793b0c458970c8d8c84a4d742c1
Ragini340 <ragini31bxr@gmail.com>
2025-01-05 18:17:55 +0530

    #CSharp_Practice Implementation to Check if Entered Number is Prime Number

---
+++ CodingQuestions/CheckForPrimeNo.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class CheckForPrimeNo
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Is entered number is prime?" + " " + IsPrime(N));
+        }
+    }
+
+}
