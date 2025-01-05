f8c0805997ed4364447d2029f5ad8b2fb8b49fd1
Ragini340 <ragini31bxr@gmail.com>
2025-01-05 15:55:11 +0530

    #CSharp_Practice Implemented Delegate POCs

---
+++ Delegate_POC/DelegateExample.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+
+namespace CSharp_Practice.Delegate_POC
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    public delegate void CallbackMethodHandler(string message);
+    public delegate void DoSomeMethodHandler(string message);
+
+    public class DelegateExample
+    {
+        public static void Main(string[] args)
+        {
+            WorkPerformedHandler workPerformedHandler = new WorkPerformedHandler(Manager_WorkPerformed);
+            workPerformedHandler(5, WorkType.Golf);
+
+            DelegateExample delegateExample = new DelegateExample();
+            CallbackMethodHandler callbackMethodHandler = new CallbackMethodHandler(delegateExample.CallbackMethod);
+            DoSomework(callbackMethodHandler);
+
+            //Example to understand Delegate class important properties and method 
+            Delegates delegates = new Delegates();
+            DoSomeMethodHandler doSomeMethodHandler = new DoSomeMethodHandler(delegates.DoSomework);
+            MethodInfo method = doSomeMethodHandler.Method;
+            object target = doSomeMethodHandler.Target;
+            Delegate[] invocationList = doSomeMethodHandler.GetInvocationList();
+            Console.WriteLine($"Method property: {method}");
+            Console.WriteLine($"Target property: {target}");
+
+            foreach (var item in invocationList)
+            {
+                Console.WriteLine($"InvocationList:- {item}");
+            }
+        }
+
+        public static void Manager_WorkPerformed(int workHours, WorkType wType)
+        {
+            Console.WriteLine("Work performed by EventHandler");
+            Console.WriteLine($"Work hours:- {workHours}, Work type:- {wType}");
+        }
+        public static void DoSomework(CallbackMethodHandler del)
+        {
+            Console.WriteLine("Processing some task");
+            del("Ragini");
+        }
+        public void CallbackMethod(string message)
+        {
+            Console.WriteLine("CallbackMethod executed");
+            Console.WriteLine($"Hello: {message}, Good Morning!!");
+        }
+    }
+
+    public enum WorkType
+    {
+        Golf,
+        GotoMeetings,
+        GenerateReports,
+    }
+
+}
---
+++ Delegate_POC/Delegates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Delegate_POC
+{
+    public class Delegates
+    {
+        public void DoSomework(string message)
+        {
+            Console.WriteLine("DoSomework executed");
+            Console.WriteLine($"Hello: {message}, Good Morning!!");
+        }
+    }
+}
