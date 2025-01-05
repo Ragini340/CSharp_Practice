451c81361b52144af62d7610f3d3378500afc2f1
Ragini340 <ragini31bxr@gmail.com>
2025-01-05 15:54:18 +0530

    #CSharp_Practice Changed Package Name

--- Delegate_POC/DelegatePOC.cs
+++ Delegate_POC/DelegatePOC.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp_Practice.Delegate
+namespace CSharp_Practice.Delegate_POC
 {
     public class DelegatePOC
     {
