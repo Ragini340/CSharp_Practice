a19f91fca1dc2b1980d81d9d52b15cf9e370b8d4
Ragini340 <ragini31bxr@gmail.com>
2025-01-05 18:18:31 +0530

    #CSharp_Practice Program to find the next prime number

---
+++ CodingQuestions/CheckNearestPrimeNo.cs
@@ -0,0 +1,50 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    //Program to find the next prime number
+    public class CheckNearestPrimeNo
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static int NextPrimeNo(int num)
+        {
+            if (num <= 1)
+            {
+                return 2;
+            }
+
+            int prime = num;
+            bool nextPrime = false;
+            while (!nextPrime)
+            {
+                prime = prime + 1;
+                if (IsPrime(prime))
+                {
+                    nextPrime = true;
+                }
+            }
+            return prime;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nearest prime number is:- " + NextPrimeNo(num));
+        }
+    }
+
+}
