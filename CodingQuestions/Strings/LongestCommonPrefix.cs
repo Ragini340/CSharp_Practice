c2d935b28715dde0274015456f94c77e4cec53ac
Ragini340 <ragini31bxr@gmail.com>
2025-01-07 07:18:07 +0530

    #CSharp_Practice program to find the longest common prefix using Sorting

---
+++ CodingQuestions/Strings/LongestCommonPrefix.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharp_Practice.CodingQuestions.Strings
+{
+    //Longest Common Prefix using Sorting
+    public class LongestCommonPrefix
+    {
+        public static string FindLongestCommonPrefix(string[] arr)
+        {
+            string first = arr[0];
+            string last = arr[arr.Length - 1];
+            int minLength = Math.Min(first.Length, last.Length);
+
+            int i = 0;
+            while (i < minLength && first[i] == last[i])
+            {
+                i = i + 1;
+            }
+            return first.Substring(0, i);
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] arr = { "unpleasant", "undo", "unhappy" };
+            Array.Sort(arr);
+            Console.WriteLine("Sorted array of string in ascending order is:-");
+            foreach (string s in arr)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(FindLongestCommonPrefix(arr));
+        }
+    }
+
+}
