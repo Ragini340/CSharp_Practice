adb46bc73b15fe84910b23d60dc16d58c03d4a98
Ragini340 <ragini31bxr@gmail.com>
2025-01-08 11:38:26 +0530

    #CSharp_Practice Updated List POC

--- Collections/Generic/List/ListPOC.cs
+++ Collections/Generic/List/ListPOC.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CSharp_Practice.Indexer.Indexer_Real_Life_Example;
 
 namespace CSharp_Practice.Collections.Generic.List
 {
@@ -63,11 +59,141 @@ namespace CSharp_Practice.Collections.Generic.List
             }
 
             Console.WriteLine("\nCount of list3 is: " + list3.Count); //Count is 7
-            //list3.Insert(8, "Ragini"); //Unhandled exception. System.ArgumentOutOfRangeException: Index must be within
-                                         //the bounds of the List. (Parameter 'index')
-            //list3.InsertRange(8, list2);  //Unhandled exception. System.ArgumentOutOfRangeException: Index was out of
-                                           //range. Must be non-negative and less than the size of the collection. (Parameter 'index')
-           
+            list3.Insert(7, "Ragini"); 
+            Console.WriteLine("\nNow count of list3 is: " + list3.Count);
+            Console.WriteLine("\nAccessing list3 after InsertRange at index 7:-");
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item);
+            }
+            list3.Insert(8, "Rag");
+            Console.WriteLine("\nCount of list3 is: " + list3.Count);
+            Console.WriteLine("\nAccessing list3 after InsertRange at index 8:-");
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item);
+            }
+            
+            //Contains
+            Console.WriteLine("Is Mike exists in list 3? " +list3.Contains("Mike"));
+
+            //Remove
+            list3.Remove("Mike");
+            Console.WriteLine("\nAccessing list3 after removing item Mike from the list3:-");
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<int> list4 = new List<int>();
+            for(int i = 1; i<= 10; i++)
+            {
+                list4.Add(i);
+            }
+            Console.WriteLine("\nElements in list4 are:-");
+            foreach(int item in list4)
+            {
+                Console.WriteLine(item);
+            }
+
+            //RemoveAll(Predicate < T > match)
+            list4.RemoveAll(num => num %2 == 0);
+            Console.WriteLine("\nAfter using RemoveAll method elements in list4 are:-");
+            foreach (int item in list4)
+            {
+                Console.WriteLine(item);
+            }
+
+            //RemoveAt
+            list4.RemoveAt(list4.Count - 1);
+            Console.WriteLine("\nAfter using RemoveAt method elements in list4 are:-");
+            foreach (int item in list4)
+            {
+                Console.WriteLine(item);
+            }
+
+            //RemoveRange
+            list4.RemoveRange(1, 2); //It will remove list elements at index 1 and index 2
+            Console.WriteLine("\nAfter using RemoveRange method elements in list4 are:-");
+            foreach (int item in list4)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Clear
+            list4.Clear();
+            Console.WriteLine("\nCount in list4 is: " + list4.Count);
+
+            //Copying an array to a list 
+            string[] arr = new string[] { "India", "USA", "Pakistan" };
+            List<string> copiedListFromArray = new List<string>(arr);
+            Console.WriteLine("\nElements of copiedListFromArray:-");
+            foreach (var item in copiedListFromArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            //List collection with complex type
+            Employee employee1 = new Employee()
+            {
+                EmployeeId = 1,
+                Name = "Ragini",
+                Gender = "F",
+                Salary = 100000
+            };
+            Employee employee2 = new Employee()
+            {
+                EmployeeId = 2,
+                Name = "Bree",
+                Gender = "F",
+                Salary = 80000
+            };
+            Employee employee3 = new Employee()
+            {
+                EmployeeId = 3,
+                Name = "Gaby",
+                Gender = "F",
+                Salary = 70000
+            };
+
+            List<Employee> employeesList = new List<Employee>();
+            employeesList.Add(employee1);
+            employeesList.Add(employee2);
+            employeesList.Add(employee3);
+            Console.WriteLine("\nElements of employeesList:-");
+            foreach (var item in employeesList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Retrieving items from the employeesList by using index
+            Employee employeeAtIndexZero = employeesList[0];
+            Console.WriteLine("\nEmployee details at index 0 is:-" + "\n" + employeesList[0]);
+
+            //Retrieving all employees of employeesList using for loop
+            Console.WriteLine("\nElements of employeesList using for loop:-");
+            for (int i = 0; i < employeesList.Count; i++)
+            {
+                Console.WriteLine(employeesList[i]);
+            }
+
+            Employee employee4 = new Employee()
+            {
+                EmployeeId = 4,
+                Name = "Juli",
+                Gender = "F",
+                Salary = 60000
+            };
+            //Insert
+            employeesList.Insert(1, employee4);
+            Console.WriteLine("\nElements in employeesList after inserting employee4 details at index 1:-");
+            foreach(var item in employeesList)
+            {
+                Console.WriteLine(item);    
+            }
+               
+           // employeesList.Find(e =>( e.EmployeeId ==4 && e.Gender=="F"));
+
         }
     }
 }
