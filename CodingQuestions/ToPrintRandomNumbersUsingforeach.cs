4ae6ebd1bb8d583103b8374adac5b816dc9dc18b
Ragini340 <ragini31bxr@gmail.com>
2025-01-17 07:58:10 +0530

    #CSharp_Practice Implementation for printing ten random numbers using foreach

---
+++ CodingQuestions/ToPrintRandomNumbersUsingforeach.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    /*
+      Print ten random numbers using foreach
+   */
+    public class ToPrintRandomNumbersUsingforeach
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int randomNum = random.Next();
+                list.Add(randomNum);
+            }
+
+            Console.WriteLine("Ten random numbers are: ");
+            foreach (int num in list)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+    }
+}
