baff8a808ea3939cacd14cd0b126c4164d41027a
Ragini340 <ragini31bxr@gmail.com>
2025-01-17 08:02:34 +0530

    #CSharp_Practice Updated PartialEmployee Classes

--- OOPs/ClassAndObjects/PartialClass/PartialEmployeeOne.cs
+++ OOPs/ClassAndObjects/PartialClass/PartialEmployeeOne.cs
@@ -7,7 +7,13 @@
         public string Gender { get; set; }
         public int Salary { get; set; }
 
-        public partial void PartialMethod();
+        public partial void PartialMethod(); //If we do not implement the partial method then it will throw compile time error as 
+                                             //Partial method must have implementation part because it has accessbility modifiers.
+
+        public partial void Test()
+        {
+            Console.WriteLine("Test method has been called");
+        }
 
         public override string ToString()
         {
--- OOPs/ClassAndObjects/PartialClass/PartialEmployeeTwo.cs
+++ OOPs/ClassAndObjects/PartialClass/PartialEmployeeTwo.cs
@@ -12,10 +12,15 @@
         }
 
         //Partial method
-        public partial void PartialMethod()
+        public partial void PartialMethod()  //If we do not declare the PartialMethod in another partial class then compiler will
+                                             //throw compile time error: No defining declaration found for implementing declaration of
+                                             //PartialMethod
         {
             Console.WriteLine("\nPartial method has been called");
         }
+
+        public partial void Test();
+       
         public void PublicMethod()
         {
             Console.WriteLine("\nPublic method has been called");
