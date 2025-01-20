216b007f5e806c70e30ccead3b78e69cf5c71718
Ragini340 <ragini31bxr@gmail.com>
2025-01-20 10:41:39 +0530

    #CSharp_Practice Implemented Example for Better understanding Background and Foreground Threads

---
+++ Threads/MultiThreading/BackgroundAndForegroundThreadEx.cs
@@ -0,0 +1,44 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading
+{
+    public class BackgroundAndForegroundThreadEx
+    {
+        public readonly int maxIterations;
+
+        public BackgroundAndForegroundThreadEx(int maxIterations)
+        {
+            this.maxIterations = maxIterations;
+        }
+
+        public void RunLoop()
+        {
+            for(int i = 0; i < maxIterations; i++)
+            {
+                Console.WriteLine("{0} count: {1}", Thread.CurrentThread.IsBackground ? "Background Thread" : "Foreground Thread", i);
+                Thread.Sleep(250);
+            }
+            Console.WriteLine("{0} finished counting", Thread.CurrentThread.IsBackground ? "Background Thread" : "Foreground Thread"); 
+        }
+
+        public static void Main(string[] args)
+        {
+            BackgroundAndForegroundThreadEx foregroundTest = new BackgroundAndForegroundThreadEx(5);
+            Console.WriteLine("Foreground thread started");
+            Thread foregroundThread = new Thread(new ThreadStart(foregroundTest.RunLoop));
+            Console.WriteLine("Foreground thread ended");
+
+            BackgroundAndForegroundThreadEx backgroundTest = new BackgroundAndForegroundThreadEx(50);
+            Console.WriteLine("Background thread started");
+            Thread backgroundThread = new Thread(new ThreadStart(backgroundTest.RunLoop))
+            {
+                IsBackground = true
+            };
+            Console.WriteLine("Background thread ended");
+
+            foregroundThread.Start();
+            backgroundThread.Start();
+            //Thread.Sleep(10000);
+            Console.WriteLine("Main thread exited");
+        }
+
+    }
+}
