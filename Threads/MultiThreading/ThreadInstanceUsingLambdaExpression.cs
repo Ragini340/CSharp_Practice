4654eb3e64546c87ab9c1d57e292b3d7b2590999
Ragini340 <ragini31bxr@gmail.com>
2025-01-22 15:16:50 +0530

    #CSharp_Practice Implemented Thread Instance Using Lambda Expression

---
+++ Threads/MultiThreading/ThreadInstanceUsingLambdaExpression.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading
+{
+    public class ThreadInstanceUsingLambdaExpression
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+
+            Thread thread1 = new Thread(() =>
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine("Method1: " + i);
+                }
+            });
+
+            Thread thread2 = new Thread(() =>
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    Console.WriteLine("Method2: " + i);
+                }
+            });
+
+            thread1.Start();
+            thread2.Start();
+            Console.WriteLine("Main thread ended");
+        }
+
+    }
+}
