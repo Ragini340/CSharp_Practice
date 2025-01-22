c31517b65ea930e4d9e7c794acb563ab17364cd4
Ragini340 <ragini31bxr@gmail.com>
2025-01-22 15:14:59 +0530

    #CSharp_Practice Implemented Thread Instance Using Anonymous Method

---
+++ Threads/MultiThreading/ThreadInstanceUsingAnonymousMethod.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading
+{
+    public class ThreadInstanceUsingAnonymousMethod
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+
+            Thread thread1 = new Thread(delegate ()
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    Console.WriteLine("Method1: " + i);
+                }
+            });
+
+            Thread thread2 = new Thread(delegate ()
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine("Method2: " + i);
+                }
+            });
+
+            thread1.Start(); //Here, Start method is invoking the delegate ThreadStart
+            thread2.Start();
+            Console.WriteLine("Main thread ended");
+        }
+
+    }
+}
