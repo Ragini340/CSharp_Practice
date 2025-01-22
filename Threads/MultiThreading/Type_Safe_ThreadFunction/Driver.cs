29cf7cd2211760af8c970cd6a2bb16420232349e
Ragini340 <ragini31bxr@gmail.com>
2025-01-22 15:05:47 +0530

    #CSharp_Practice Implemented Type-Safe Thread Function With Passing One Parameter

---
+++ Threads/MultiThreading/Type_Safe_ThreadFunction/Driver.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Type_Safe_ThreadFunction
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+            int Max = 20;
+            HelperNumber obj = new HelperNumber(Max);
+            Thread thread1 = new Thread(new ThreadStart(obj.DisplayNumbers));
+            thread1.Start();
+            Console.WriteLine("Main thread ended");
+        }
+
+    }
+}
---
+++ Threads/MultiThreading/Type_Safe_ThreadFunction/HelperNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Type_Safe_ThreadFunction
+{
+    public class HelperNumber
+    {
+        int _Number;
+        public HelperNumber(int Number)
+        {
+            _Number = Number;
+        }
+
+        public void DisplayNumbers()
+        {
+            for (int i = 1; i <= _Number; i++)
+            {
+                Console.WriteLine("Method1 Type-Safe: " + i);
+            }
+        }
+
+    }
+}
