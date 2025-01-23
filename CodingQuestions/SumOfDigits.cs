dc168d6423e5d8d33fee03bd2a297c7289634c05
Ragini340 <ragini31bxr@gmail.com>
2025-01-23 15:13:10 +0530

    #CSharp_Practice Implemented Sum of Digits of Given Number

---
+++ CodingQuestions/SumOfDigits.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class SumOfDigits
+    {
+        public static void FindSumOfDigits(int num)
+        {
+            int sum = 0;
+            int remainder;
+
+            while (num > 0)
+            {
+                remainder = num % 10; 
+                num = num / 10;
+                sum = sum + remainder;   
+            }
+
+            Console.Write("Sum of the digits of the entered number is: " + sum);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+            SumOfDigits.FindSumOfDigits(number);
+        }
+
+    }
+}
