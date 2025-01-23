4bce10ad5ba39265798e6e4b330c09a9eb6cd30d
Ragini340 <ragini31bxr@gmail.com>
2025-01-23 13:46:48 +0530

    #CSharp_Practice Implemented Count of Occurrences of Each Character in a String

---
+++ CodingQuestions/Strings/CountOccurrencesOfEachCharacterInString.cs
@@ -0,0 +1,39 @@
+﻿namespace CSharp_Practice.CodingQuestions.Strings
+{
+    public class CountOccurrencesOfEachCharacterInString
+    {
+        public static void FindCountOfOccurrencesOfEachCharacter(string str)
+        {
+            Dictionary<char, int> count = new Dictionary<char, int>();
+            char[] arr = str.ToCharArray();
+
+            foreach (char ch in arr)
+            {
+                if (ch != ' ')
+                {
+                    if (count.ContainsKey(ch))
+                    {
+                        count[ch]++;
+                    }
+                    else
+                    {
+                        count[ch] = 1;
+                    }
+                }
+            }
+
+            foreach (var item in count)
+            {
+                Console.WriteLine("Occurrences of " + item.Key + " = " + item.Value);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string str = Console.ReadLine();
+            CountOccurrencesOfEachCharacterInString.FindCountOfOccurrencesOfEachCharacter(str);
+        }
+
+    }
+}
