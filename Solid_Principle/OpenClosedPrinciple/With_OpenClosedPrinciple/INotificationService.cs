acd0c5df81329c5d20042678f87a4cb56b407910
Ragini340 <ragini31bxr@gmail.com>
2025-01-24 08:25:07 +0530

    #CSharp_Practice Updated Open Closed Principle Example

--- Solid_Principle/OpenClosedPrinciple/With_OpenClosedPrinciple/INotificationService.cs
+++ Solid_Principle/OpenClosedPrinciple/With_OpenClosedPrinciple/INotificationService.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Practice.Solid_Principle.OpenClosedPrinciple.With_OpenClosedPrinciple
+﻿namespace CSharp_Practice.Solid_Principle.OpenClosedPrinciple.With_OpenClosedPrinciple
 {
     //Open Closed Principle states that software entities(classes, modules, functions, etc.) should be open for extension,
     //but closed for modification, which means you should be able to extend a class behavior, without modifying it.
-    public interface NotificationService
+    public interface INotificationService
     {
         public void SendOTP(string medium);
         public void SendTransactionNotification(string medium);
--- Solid_Principle/OpenClosedPrinciple/With_OpenClosedPrinciple/SMSNotification.cs
+++ Solid_Principle/OpenClosedPrinciple/With_OpenClosedPrinciple/SMSNotification.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Practice.Solid_Principle.OpenClosedPrinciple.With_OpenClosedPrinciple
+﻿namespace CSharp_Practice.Solid_Principle.OpenClosedPrinciple.With_OpenClosedPrinciple
 {
     //Open Closed Principle states that software entities(classes, modules, functions, etc.) should be open for extension,
     //but closed for modification, which means you should be able to extend a class behavior, without modifying it.
-    public class MobileNotification : NotificationService
+    public class SMSNotification : INotificationService
     {
         public void SendOTP(string medium)
         {
