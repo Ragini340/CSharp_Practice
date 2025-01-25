55973cb97eaf73152fd5c1dd543d1eef5444e167
Ragini340 <ragini31bxr@gmail.com>
2025-01-25 18:36:11 +0530

    #CSharp_Practice Implemented Real-Time Example Without Using Thread Synchronization

---
+++ Threads/MultiThreading/Synchronization/Real_Time_Example/WithoutSynchronization/BookMyShow.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Real_Time_Example.WithoutSynchronization
+{
+    public class BookMyShow
+    {
+        int AvailableTickets = 3;
+        static int i = 1;
+        static int j = 2;
+        static int k = 3;
+
+        public void BookTicket(string name, int wantedtickets)
+        {
+            if (wantedtickets <= AvailableTickets)
+            {
+                Console.WriteLine(wantedtickets + " booked to " + name);
+                AvailableTickets = AvailableTickets - wantedtickets;
+            }
+            else
+            {
+                Console.WriteLine("No tickets Available to book");
+            }
+        }
+
+        public void TicketBookig()
+        {
+            string name = Thread.CurrentThread.Name;
+            if (name.Equals("Thread1"))
+            {
+                BookTicket(name, i);
+            }
+            else if (name.Equals("Thread2"))
+            {
+                BookTicket(name, j);
+            }
+            else
+            {
+                BookTicket(name, k);
+            }
+        }
+
+    }
+}
---
+++ Threads/MultiThreading/Synchronization/Real_Time_Example/WithoutSynchronization/Driver.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Real_Time_Example.WithoutSynchronization
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            BookMyShow bookMyShow = new BookMyShow();
+            Thread thread1 = new Thread(bookMyShow.TicketBookig)
+            {
+                Name = "Thread1"
+            };
+            Thread thread2 = new Thread(bookMyShow.TicketBookig)
+            {
+                Name = "Thread2"
+            };
+            Thread thread3 = new Thread(bookMyShow.TicketBookig)
+            {
+                Name = "Thread3"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+    }
+}
