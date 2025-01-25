d8e215159b212e7c66be1b5e2480a4ba6109fa30
Ragini340 <ragini31bxr@gmail.com>
2025-01-25 18:49:20 +0530

    #CSharp_Practice Implemented Real-Time Example With Using Thread Synchronization

---
+++ Threads/MultiThreading/Synchronization/Real_Time_Example/WithSynchronization/Driver.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Real_Time_Example.WithSynchronization
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            BookMyShow bookMyShow = new BookMyShow();
+            Thread t1 = new Thread(bookMyShow.TicketBookig)
+            {
+                Name = "Thread1"
+            };
+            Thread t2 = new Thread(bookMyShow.TicketBookig)
+            {
+                Name = "Thread2"
+            };
+            Thread t3 = new Thread(bookMyShow.TicketBookig)
+            {
+                Name = "Thread3"
+            };
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+
+    }
+}
