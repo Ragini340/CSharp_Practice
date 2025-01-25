7f766917d1ec619a2db4854c2369c70bfb59f177
Ragini340 <ragini31bxr@gmail.com>
2025-01-25 16:23:11 +0530

    #CSharp_Practice Implemented Thread Synchronization Example

---
+++ Threads/MultiThreading/Synchronization/ThreadSynchronizationEx.cs
@@ -0,0 +1,38 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization
+{
+    public class ThreadSynchronizationEx
+    {
+        static object lockObject = new object();
+        public static void Method1()
+        {
+            lock (lockObject)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("Current thread is: " + Thread.CurrentThread.Name + " => " + i);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Method1)
+            {
+                Name = "Thread1"
+            };
+            Thread thread2 = new Thread(Method1)
+            {
+                Name = "Thread2"
+            };
+            Thread thread3 = new Thread(Method1)
+            {
+                Name = "Thread3"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+    }
+}
