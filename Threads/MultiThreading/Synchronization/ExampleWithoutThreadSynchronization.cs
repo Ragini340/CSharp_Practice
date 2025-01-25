cb12ac20edc37271026ee9a7ba9d329e95231378
Ragini340 <ragini31bxr@gmail.com>
2025-01-25 16:21:41 +0530

    #CShrap_Practice Example Without Thread Synchronization

---
+++ Threads/MultiThreading/Synchronization/ExampleWithoutThreadSynchronization.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization
+{
+    public class ExampleWithoutThreadSynchronization
+    {
+        public static void Method1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Current thread is: " + Thread.CurrentThread.Name + " => " +i);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Method1)
+            {
+                Name = "Thread1"
+            };
+            Thread thread2 = new Thread(Method1)
+            {
+                Name = "Thread2"
+            };
+            Thread thread3 = new Thread(Method1)
+            {
+                Name = "Thread3"
+            };
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+
+    }
+}
