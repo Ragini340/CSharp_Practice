cb4e99cabbc1b76d6c820f345e703abbe2cc4045
Ragini340 <ragini31bxr@gmail.com>
2025-01-25 16:16:54 +0530

    #CSharp_Practice Implementation for Retrieving Data from a Thread Function using Callback Method

---
+++ Threads/MultiThreading/RetrieveData_From_ThreadFunction/Driver.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.RetrieveData_From_ThreadFunction
+{
+    public class Driver
+    {
+        public static int ResultCallBackMethod(int result)
+        {
+            int res = result;
+            Console.WriteLine(res);
+            return res;
+        }
+
+        public static void Main(string[] args)
+        {
+            ResultCallbackDelegate resultCallbackDelegate = new ResultCallbackDelegate(ResultCallBackMethod);
+            int number = 10;
+            NumberHelper obj = new NumberHelper(number, resultCallbackDelegate);
+            Thread thread1 = new Thread(new ThreadStart(obj.CalculateSum));
+            thread1.Start();
+        }
+
+    }
+}
---
+++ Threads/MultiThreading/RetrieveData_From_ThreadFunction/NumberHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.RetrieveData_From_ThreadFunction
+{
+    public delegate int ResultCallbackDelegate(int Results);
+    public class NumberHelper
+    {
+        private int _Number;
+        private ResultCallbackDelegate _resultCallbackDelegate;
+
+        public NumberHelper(int Number, ResultCallbackDelegate resultCallbackDelagate)
+        {
+            _Number = Number;
+            _resultCallbackDelegate = resultCallbackDelagate;
+        }
+
+        public void CalculateSum()
+        {
+            int result = 0;
+            for (int i = 1; i <= _Number; i++)
+            {
+                result = result + i;
+            }
+
+            if (_resultCallbackDelegate != null)
+            {
+                _resultCallbackDelegate(result);
+            }
+        }
+
+    }
+}
