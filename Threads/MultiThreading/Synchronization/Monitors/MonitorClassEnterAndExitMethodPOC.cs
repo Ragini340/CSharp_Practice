06ec57047534f17b8996e606967b0b5552ec3c65
Ragini340 <ragini31bxr@gmail.com>
2025-01-26 10:26:35 +0530

    #CSharp_Practice Synchronization using Monitor class Enter and Exit method to protect a shared resource in a multithreaded environment

---
+++ Threads/MultiThreading/Synchronization/Monitors/MonitorClassEnterAndExitMethodPOC.cs
@@ -0,0 +1,44 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Monitors
+{
+    public class MonitorClassEnterAndExitMethodPOC
+    {
+        private static object lockPrintNumbers = new object();
+
+        public static void PrintNumbers()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " trying to enter into the critical section");
+            try
+            {
+                Monitor.Enter(lockPrintNumbers);
+                Console.WriteLine(Thread.CurrentThread.Name + " entered into the critical section");
+                for (int i = 1; i < 6; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            finally
+            {
+                Monitor.Exit(lockPrintNumbers);
+                Console.WriteLine(Thread.CurrentThread.Name + " exits from critical section");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+                threads[i] = new Thread(PrintNumbers)
+                {
+                    Name = "Child Thread " + i
+                };
+            }
+
+            foreach (Thread th in threads)
+            {
+                th.Start();
+            }
+        }
+
+    }
+}
