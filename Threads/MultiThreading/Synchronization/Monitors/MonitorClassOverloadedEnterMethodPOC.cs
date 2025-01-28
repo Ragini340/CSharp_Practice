36f760aaa81177b9ca56e5084ece1160a8a6ed59
Ragini340 <ragini31bxr@gmail.com>
2025-01-28 11:49:19 +0530

    #CSharp_Practice Implemented Monitor Class Overloaded Enter Method POC

---
+++ Threads/MultiThreading/Synchronization/Monitors/MonitorClassOverloadedEnterMethodPOC.cs
@@ -0,0 +1,56 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Monitors
+{
+    public class MonitorClassOverloadedEnterMethodPOC
+    {
+        private static object lockPrintNumber = new object();
+        public static void PrintNumbers()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " trying to enter into the critical section");
+            bool IsLockTaken = false; //If we pass true then it will throw Unhandled exception. Unhandled exception.
+                                      //Unhandled exception. System.ArgumentException: Argument must be initialized to
+                                      //false (Parameter 'lockTaken')
+            try
+            {
+                Console.WriteLine("IsEntered value is? " + Monitor.IsEntered(lockPrintNumber));
+                Monitor.Enter(lockPrintNumber, ref IsLockTaken);
+                Console.WriteLine("Value of IsLockTaken? " + IsLockTaken);
+                Console.WriteLine("IsEntered value is? " + Monitor.IsEntered(lockPrintNumber));
+
+                if (IsLockTaken)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " entered into the critical section");
+                    for (int i = 1; i < 6; i++)
+                    {
+                        Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+                    }
+                }
+            }
+            finally
+            {
+                if (IsLockTaken)
+                {
+                    Monitor.Exit(lockPrintNumber);
+                    Console.WriteLine(Thread.CurrentThread.Name + " exits from critical section");
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+
+                threads[i] = new Thread(PrintNumbers)
+                {
+                    Name = "Child Thread " + i
+                };
+            }
+            foreach (Thread th in threads)
+            {
+                th.Start();
+            }
+        }
+
+    }
+}
