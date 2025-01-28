93008c0f260bc2b7f9a9af853e97b5d1ad8b4ef7
Ragini340 <ragini31bxr@gmail.com>
2025-01-28 11:50:25 +0530

    #CSharp_Practice Implemented Monitor Class TryEnter POC

---
+++ Threads/MultiThreading/Synchronization/Monitors/MonitorClassTryEnterPOC.cs
@@ -0,0 +1,56 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Monitors
+{
+    public class MonitorClassTryEnterPOC
+    {
+        private static object lockPrintNumbers = new object();
+        public static void PrintNumbers()
+        {
+            TimeSpan timeout = TimeSpan.FromMilliseconds(1000);
+            bool lockTaken = false;
+
+            try
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " trying to enter into the critical section");
+                Monitor.TryEnter(lockPrintNumbers, timeout, ref lockTaken);
+                if (lockTaken)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " entered into the critical section");
+                    for (int i = 1; i < 11; i++)
+                    {
+                        Thread.Sleep(100);  
+                        Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " lock was not acquired");
+                }
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(lockPrintNumbers);
+                    Console.WriteLine(Thread.CurrentThread.Name + " exits from critical section");
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+                threads[i] = new Thread(PrintNumbers)
+                {
+                    Name = "Child Thread " + i
+                };
+            }
+            foreach (Thread th in threads)
+            {
+                th.Start();
+            }
+        }
+
+    }
+}
