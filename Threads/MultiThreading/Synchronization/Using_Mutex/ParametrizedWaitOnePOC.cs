4311a741ba8b2db210fbe9997a190b84c1f679b4
Ragini340 <ragini31bxr@gmail.com>
2025-01-31 17:24:56 +0530

    #CSharp_Practice Updated Parametrized WaitOne Method

--- Threads/MultiThreading/Synchronization/Using_Mutex/ParametrizedWaitOnePOC.cs
+++ Threads/MultiThreading/Synchronization/Using_Mutex/ParametrizedWaitOnePOC.cs
@@ -2,22 +2,25 @@
 {
     public class ParametrizedWaitOnePOC
     {
-        private static Mutex mutex = new Mutex();
+        private Mutex mutex = new Mutex();
 
-        public static void MutexWaitOneDemo()
+        public void MutexWaitOneDemo()
         {
             Console.WriteLine(Thread.CurrentThread.Name + " wants to enter critical section for processing");
             /*Note: The Mutex Class is inherited from WaitHandle class and the WaitHandle class provides the WaitOne() method which 
               we need to call to lock the resource. A Mutex can only be released from the same thread which obtained it.*/
 
-            if (mutex.WaitOne(1000))
+            //In below if I use mutex.WaitOne(1000) then within 1000 ms any thread can access and complete its task.
+            //And remaining threads will go into else block and will not acquire the lock and neither accesss the resources.
+            if (mutex.WaitOne(1800)) //If I use mutex.WaitOne() without passing time then all three threads will acquire the lock on
+                                     //the resources and complete its task one by one.
             {
                 try
                 {
                     Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is processing now");
                     for (int i = 1; i < 6; i++)
                     {
-                        Thread.Sleep(1000); //If we don't use Sleep method here then all three threads will complete it's task.
+                        Thread.Sleep(800); //If we don't use Sleep method here then all three threads will complete its task.
                         Console.WriteLine(Thread.CurrentThread.Name + " " + i);
                     }
                     Console.WriteLine("Exit: " + Thread.CurrentThread.Name + " is completed its task");
@@ -36,9 +39,10 @@
 
         public static void Main(string[] args)
         {
+            ParametrizedWaitOnePOC parametrizedWaitOnePOC = new ParametrizedWaitOnePOC();
             for (int i = 1; i <= 3; i++)
             {
-                Thread thread = new Thread(MutexWaitOneDemo)
+                Thread thread = new Thread(parametrizedWaitOnePOC.MutexWaitOneDemo)
                 {
                     Name = "Thread " + i
                 };
