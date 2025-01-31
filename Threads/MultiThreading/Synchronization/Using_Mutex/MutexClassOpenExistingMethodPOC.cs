986a42bc0fdcd9590a5c945370be6b7987a5adca
Ragini340 <ragini31bxr@gmail.com>
2025-01-31 17:35:28 +0530

    #CSharp_Practice Mutex Class OpenExistingMethod POC

---
+++ Threads/MultiThreading/Synchronization/Using_Mutex/MutexClassOpenExistingMethodPOC.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Using_Mutex
+{
+    /*OpenExisting(string name): This method is used to open the specified named mutex if it already exists. It returns an object 
+      that represents the named system mutex. Here, the parameter name specifies the name of the system mutex to open. It will throw
+      ArgumentException if the name is an empty string or name is longer than 260 characters. It will throw ArgumentNullException if
+      the name is null.*/
+    /*In the below example, we use the OpenExisting method of the Mutex Class. If this method throws an exception, the specified
+      named Mutex does not exist or is inaccessible.*/
+    public class MutexClassOpenExistingMethodPOC
+    {
+        private static Mutex _mutex;
+
+        public Mutex IsSingleInstance()
+        {
+            try
+            {
+              return Mutex.OpenExisting("MyMutex");            
+            }
+            catch
+            {
+                _mutex = new Mutex(true, "MyMutex");
+                return _mutex;
+            }
+            return _mutex;
+        }
+
+        public static void Main(string[] args)
+        {
+            MutexClassOpenExistingMethodPOC obj = new MutexClassOpenExistingMethodPOC();
+            if (obj.IsSingleInstance != null)
+            {
+                Console.WriteLine("Mutex name MyMutex is available"); 
+            }
+            else
+            {
+                Console.WriteLine("Mutex name MyMutex is not available");
+            }
+        }
+
+    }
+}
