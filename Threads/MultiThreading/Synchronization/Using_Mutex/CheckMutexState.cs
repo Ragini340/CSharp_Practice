fd732b93f8e2a1ff5f1b0568cd08eb37fd254344
Ragini340 <ragini31bxr@gmail.com>
2025-01-31 17:31:18 +0530

    #CSharp_Practice Mutex State POC

---
+++ Threads/MultiThreading/Synchronization/Using_Mutex/CheckMutexState.cs
@@ -0,0 +1,43 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Using_Mutex
+{
+    public class CheckMutexState
+    {
+        private Mutex _mutex;
+        public CheckMutexState(bool initiallyOwned, string? name)
+        {
+            _mutex = new Mutex(true, "MyMutex");
+        }
+
+        public void CheckAllMutexFeature()
+        {
+            try
+            {
+                var mutexObj = Mutex.OpenExisting("MyMutex1");
+                Console.WriteLine("Mutex is available with name MyMutex1");
+            }
+            catch (WaitHandleCannotBeOpenedException ex)
+            {
+                Console.WriteLine("Mutext is not available with name MyMutex1");
+            }
+
+            //TryOpenExisting(string name, out Mutex result):-
+            var mutexTryOpenObj = Mutex.TryOpenExisting("MyMutex1", out Mutex? result);
+            Console.WriteLine(mutexTryOpenObj);
+
+            var mutexTryOpen = Mutex.TryOpenExisting("MyMutex", out Mutex? ressult1);
+            Console.WriteLine(mutexTryOpen);
+        }
+
+        ~CheckMutexState()
+        {
+            _mutex.Dispose();
+        }
+
+        public static void Main(string[] args)
+        {
+            CheckMutexState obj = new CheckMutexState(true, "MyMutex");
+            obj.CheckAllMutexFeature();
+        }
+
+    }
+}
