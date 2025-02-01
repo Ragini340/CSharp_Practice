5a9cdc7a0962dbcc46ba36d8216b1b496b749c10
Ragini340 <ragini31bxr@gmail.com>
2025-02-01 18:42:15 +0530

    #CSharp_Practice Implemented Example to Understand Deadlock

---
+++ Threads/MultiThreading/Synchronization/Deadlock/Example/Account.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Deadlock.Example
+{
+    public class Account
+    {
+        public int Id { get; }
+        private double Balance { get; set; }
+        public Account(int id, double balance)
+        {
+            Id = id;
+            Balance = balance;
+        }
+
+        public void WithdrawMoney(double amount)
+        {
+            Balance = Balance - amount;
+        }
+
+        public void DepositMoney(double amount)
+        {
+            Balance = Balance + amount;
+        }
+
+    }
+}
---
+++ Threads/MultiThreading/Synchronization/Deadlock/Example/AccountManager.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Deadlock.Example
+{
+    public class AccountManager
+    {
+        private Account FromAccount;
+        private Account ToAccount;
+        private double TransferAmount;
+        public AccountManager(Account AccountFrom, Account AccountTo, double AmountTransfer)
+        {
+            FromAccount = AccountFrom;
+            ToAccount = AccountTo;
+            TransferAmount = AmountTransfer;
+        }
+
+        public void FundTransfer()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {FromAccount.Id}");
+            lock (FromAccount)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {FromAccount.Id}");
+                Console.WriteLine($"{Thread.CurrentThread.Name} doing some work");
+                //Thread.Sleep(1000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {ToAccount.Id}");
+                lock (ToAccount)
+                {
+                    FromAccount.WithdrawMoney(TransferAmount);
+                    ToAccount.DepositMoney(TransferAmount);
+                }
+            }
+        }
+
+    }
+}
