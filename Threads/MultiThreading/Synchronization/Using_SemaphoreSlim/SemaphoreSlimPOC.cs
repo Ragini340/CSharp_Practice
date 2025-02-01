e4dcae14643f2fb1c1f00079206e4ddcc6356f1c
Ragini340 <ragini31bxr@gmail.com>
2025-02-01 18:37:40 +0530

    #CSharp_Practice POC of Synchronization Using SemaphoreSlim

---
+++ Threads/MultiThreading/Synchronization/Using_SemaphoreSlim/SemaphoreSlimPOC.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Using_SemaphoreSlim
+{
+    public class SemaphoreSlimPOC
+    {
+        //Only 3 threads can access resource simulteniously
+        public SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(initialCount: 3);
+
+        public void SemaphoreSlimFunction()
+        {
+            Task[] task = new Task[5];
+            Console.WriteLine(Thread.CurrentThread.Name + " wants to enter into critical section for processing");
+            _semaphoreSlim.Wait();
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+            }
+            Console.WriteLine("Exit: " + Thread.CurrentThread.Name + " is completed its task");
+
+            _semaphoreSlim.Release();
+            Console.WriteLine(Thread.CurrentThread.Name + " exits from critical section");
+        }
+
+        public static void Main(string[] args)
+        {
+            SemaphoreSlimPOC obj = new SemaphoreSlimPOC();
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread thread = new Thread(obj.SemaphoreSlimFunction)
+                {
+                    Name = "Thread " + i
+                };
+                thread.Start();
+            }
+        }
+
+    }
+}
