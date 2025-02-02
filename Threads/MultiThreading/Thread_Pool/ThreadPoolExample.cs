012ba6e4c3fabee0ceac30e8478d364a3e380378
Ragini340 <ragini31bxr@gmail.com>
2025-02-02 19:27:13 +0530

    #CSharp_Practice Example to Understand Thread Pooling

---
+++ Threads/MultiThreading/Thread_Pool/ThreadPoolExample.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Thread_Pool
+{
+    //https://stackoverflow.com/questions/16705313/why-threadpool-thread-doesnt-print-anything-to-console-unless-main-thread-print
+    public class ThreadPoolExample
+    {
+        public void MyMethod(object obj)
+        {
+            Thread thread = Thread.CurrentThread;
+            string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}, Thread ID: {thread.ManagedThreadId}";
+            Console.WriteLine(message);
+        }
+
+        public static void Main(string[] args)
+        {
+            ThreadPoolExample threadPoolExample = new ThreadPoolExample();
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(threadPoolExample.MyMethod));
+            }
+            Console.Read();
+        }
+
+    }
+}
