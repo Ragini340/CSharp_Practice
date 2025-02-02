be20b865523541131003ad3ad88a5aea2d6927fa
Ragini340 <ragini31bxr@gmail.com>
2025-02-02 21:13:50 +0530

    #CSharp_Practice Performance testing using and without using Thread Pool

---
+++ Threads/MultiThreading/Performance_Testing/PerfTestingUsingAndWithoutUsingThreadPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace CSharp_Practice.Threads.MultiThreading.Performance_Testing
+{
+    public class PerfTestingUsingAndWithoutUsingThreadPool
+    {
+        public static void MethodWithThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(Test);
+                thread.Start();
+            }
+        }
+
+        public static void MethodWithThreadPool()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Test));
+            }
+        }
+
+        public static void Test(object obj) { }
+
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MethodWithThread();
+                MethodWithThreadPool();
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Execution using Thread");
+            stopwatch.Start();
+            MethodWithThread();
+            stopwatch.Stop();
+            Console.WriteLine("Time consumed by MethodWithThread is : " + stopwatch.ElapsedTicks.ToString());
+            stopwatch.Reset();
+
+            Console.WriteLine("Execution using Thread Pool");
+            stopwatch.Start();
+            MethodWithThreadPool();
+            stopwatch.Stop();
+            Console.WriteLine("Time consumed by MethodWithThreadPool is : " + stopwatch.ElapsedTicks.ToString());
+            Console.Read();
+        }
+
+    }
+}
