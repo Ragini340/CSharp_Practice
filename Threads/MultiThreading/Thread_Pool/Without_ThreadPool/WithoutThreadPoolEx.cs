af66cba8608c006b0fd8fdf468c5bad6812d494b
Ragini340 <ragini31bxr@gmail.com>
2025-02-02 20:40:41 +0530

    #CSharp_Practice Example of Thread class to create the threads and invoke the methods without using ThreadPool

---
+++ Threads/MultiThreading/Thread_Pool/Without_ThreadPool/WithoutThreadPoolEx.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp_Practice.Threads.MultiThreading.Thread_Pool.Without_ThreadPool
+{
+    public class WithoutThreadPoolEx
+    {
+        public void MyMethod(object obj)
+        {
+            Thread thread = Thread.CurrentThread;
+            string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}, Thread ID: {thread.ManagedThreadId}";
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            WithoutThreadPoolEx withoutThreadPoolEx = new WithoutThreadPoolEx();
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(withoutThreadPoolEx.MyMethod)
+                {
+                    Name = "Thread" + i
+                };
+                thread.Start();
+            }
+        }
+
+    }
+}
