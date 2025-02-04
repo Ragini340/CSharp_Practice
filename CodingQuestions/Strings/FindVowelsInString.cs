1fe570d609577b29ea2b4bf9e56ff5df4c67e44c
Ragini340 <ragini31bxr@gmail.com>
2025-02-04 10:15:26 +0530

    #CSharp_Practice Implementation for Finding Vowels in a String

---
+++ CodingQuestions/Strings/FindVowelsInString.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharp_Practice.CodingQuestions.Strings
+{
+    public class FindVowelsInString
+    {
+        public static int FindVowels(String str)
+        {
+            int vowelCount = 0;
+
+            if (str != null)
+            {
+                var vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+                foreach (char ch in str.ToLower())
+                {
+                    if (vowels.Contains(ch))
+                    {
+                        vowelCount++;
+                    }
+                }
+            }
+
+            return vowelCount;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Vowels count is: " + FindVowels(str));
+        }
+
+    }
+}
