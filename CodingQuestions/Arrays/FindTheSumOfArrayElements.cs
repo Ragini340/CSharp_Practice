928a482b3dc3704b9e22335b51ffc4839888c033
Ragini340 <ragini31bxr@gmail.com>
2025-02-04 08:19:21 +0530

    #CSharp_Practice Implementation for Finding Sum of Array Elements

---
+++ CodingQuestions/Arrays/FindTheSumOfArrayElements.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharp_Practice.CodingQuestions.Arrays
+{
+    public class FindTheSumOfArrayElements
+    {
+        public void SumOfArrElements(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+            }
+
+            Console.WriteLine("Sum of the entered array elements is: " + sum);
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4 };
+
+            FindTheSumOfArrayElements obj = new FindTheSumOfArrayElements();
+            obj.SumOfArrElements(arr);
+        }
+
+    }
+}
