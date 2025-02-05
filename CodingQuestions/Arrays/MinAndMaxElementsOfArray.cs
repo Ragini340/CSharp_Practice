1b68dcff8b2bb052b3f8f67df087e1dc7c4d3992
Ragini340 <ragini31bxr@gmail.com>
2025-02-05 16:21:14 +0530

    #CSharp_Practice Program to Find Min & Max Element in an Array

---
+++ CodingQuestions/Arrays/MinAndMaxElementsOfArray.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp_Practice.CodingQuestions.Arrays
+{
+    public class MinAndMaxElementsOfArray
+    {
+        public static void FindMinMaxArrElements(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Max: " + max);
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = { 10, 30, 40, 100, 170, 50, 20, 60 };
+            FindMinMaxArrElements(arr);
+        }
+
+    }
+}
