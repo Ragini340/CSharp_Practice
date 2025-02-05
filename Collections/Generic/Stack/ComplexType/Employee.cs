40a3ca2050333c5d5f05e2effff847eeeedace45
Ragini340 <ragini31bxr@gmail.com>
2025-02-05 13:47:42 +0530

    #CSharp_Practice Implemented Generic Stack Collection Class with Complex Type

---
+++ Collections/Generic/Stack/ComplexType/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Indexer.Indexer_Real_Life_Example
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+
+        public override string ToString()
+        {
+            return "EmployeeId: " + this.EmployeeId + " Name: " + this.Name + " Gender: " + this.Gender + " Salary: " + this.Salary;
+        }
+    }
+}
---
+++ Collections/Generic/Stack/ComplexType/StackPOC.cs
@@ -0,0 +1,90 @@
+﻿namespace CSharp_Practice.Collections.Generic.Stack.ComplexType
+{
+    public class StackPOC
+    {
+        public static void Main(string[] args)
+        {
+            Employee emp1 = new Employee()
+            {
+                EmployeeId = 100,
+                Name = "Mike",
+                Gender = "Male",
+                Salary = 10000
+            };
+            Employee emp2 = new Employee()
+            {
+                EmployeeId = 101,
+                Name = "Susan",
+                Gender = "Female",
+                Salary = 20000
+            };
+            Employee emp3 = new Employee()
+            {
+                EmployeeId = 102,
+                Name = "Carl",
+                Gender = "Male",
+                Salary = 30000
+            };
+            Employee emp4 = new Employee()
+            {
+                EmployeeId = 103,
+                Name = "Bree",
+                Gender = "Female",
+                Salary = 40000
+            };
+            Employee emp5 = new Employee()
+            {
+                EmployeeId = 104,
+                Name = "Gabby",
+                Gender = "Female",
+                Salary = 50000
+            };
+
+            Stack<Employee> employees = new Stack<Employee>();
+            employees.Push(emp1);
+            employees.Push(emp2);
+            employees.Push(emp3);
+            employees.Push(emp4);
+            employees.Push(emp5);
+            Console.WriteLine("Employee's details are: ");
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine(emp);
+            }
+
+            //Pop
+            Employee e1 = employees.Pop();
+            Console.WriteLine("\n" + e1);
+            Employee e2 = employees.Pop();
+            Console.WriteLine(e2);
+            Employee e3 = employees.Pop();
+            Console.WriteLine(e3);
+            Employee e4 = employees.Pop();
+            Console.WriteLine(e4);
+            Employee e5 = employees.Pop();
+            Console.WriteLine(e5);
+
+            //Push
+            employees.Push(emp1);
+            employees.Push(emp2);
+            employees.Push(emp3);
+            employees.Push(emp4);
+            employees.Push(emp5);
+
+            //Peek
+            Employee employee = employees.Peek();
+            Console.WriteLine("\n" + employee);
+
+            //Contains
+            if (employees.Contains(emp3))
+            {
+                Console.WriteLine("\nemp3 is in stack");
+            }
+            else
+            {
+                Console.WriteLine("\nemp3 is not in stack");
+            }
+        }
+
+    }
+}
