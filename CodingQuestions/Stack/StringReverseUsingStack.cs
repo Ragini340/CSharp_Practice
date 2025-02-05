8ac10ac3ed0b7ac32708ec281ec912e864417507
Ragini340 <ragini31bxr@gmail.com>
2025-02-05 10:51:59 +0530

    #CSharp_Practice Implemented Reverse String using Stack

---
+++ CodingQuestions/Stack/StringReverseUsingStack.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp_Practice.CodingQuestions.Stack
+{
+    public class StringReverseUsingStack
+    {
+        public void ReverseString(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                stack.Push(c);   
+            }
+
+            string result = "";
+            while(stack.Count > 0)
+            {
+                  result = result +  stack.Pop();
+            }
+            Console.WriteLine(result);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input string");
+            string str = Console.ReadLine();
+            StringReverseUsingStack obj = new StringReverseUsingStack();
+            obj.ReverseString(str);
+        }
+
+    }
+}
