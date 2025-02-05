89528d7aecf09789807bcd2fa19fc1701602c648
Ragini340 <ragini31bxr@gmail.com>
2025-02-05 10:50:38 +0530

    #CSharp_Practice Implemented Reverse String using Recursion

---
+++ Recursion/StringReverseUsingRecursion.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp_Practice.Recursion
+{
+    public class StringReverseUsingRecursion
+    {
+        public static string ReverseString(string str, string result)
+        {
+            if (str.Length <= 1)
+            {
+                return result + str[str.Length - 1];
+            }
+
+            return ReverseString(str.Substring(0, str.Length - 1), result + str[str.Length - 1]);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input string");
+            string str = Console.ReadLine();
+            string result = "";
+            Console.Write(ReverseString(str, result));
+        }
+
+    }
+}
