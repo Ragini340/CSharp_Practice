00cc17158b368662d722cc3abf198283e82967ef
Ragini340 <ragini31bxr@gmail.com>
2025-02-10 18:56:32 +0530

    #CSharp_Practice Task Class POCs

---
+++ Asynchronous_Programming/Task_Class/AsyncAndAwaitPOC.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp_Practice.Asynchronous_Programming.Task_Class
+{
+    public class AsyncAndAwaitPOC
+    {
+        public async void Print()
+        {
+            Console.WriteLine("Print method started......");
+            Wait();
+            Console.WriteLine("Print method end");
+        }
+
+        private async Task Wait()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("10 seconds wait completed");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main method started......");
+            AsyncAndAwaitPOC asyncAndAwaitPOC = new AsyncAndAwaitPOC();
+            asyncAndAwaitPOC.Print();
+            Console.WriteLine("Main method end");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Asynchronous_Programming/Task_Class/AsyncWithAwaitOperatorPOC.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp_Practice.Asynchronous_Programming.Task_Class
+{
+    public class AsyncWithAwaitOperatorPOC
+    {
+        public async void Print()
+        {
+            Console.WriteLine("Print method started......");
+            await Wait();
+            Console.WriteLine("Print method end");
+        }
+
+        private async Task Wait()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("10 seconds wait completed");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main method started......");
+            AsyncWithAwaitOperatorPOC asyncWithAwaitOperator = new AsyncWithAwaitOperatorPOC();
+            asyncWithAwaitOperator.Print();
+            Console.WriteLine("Main method end");
+            Console.ReadKey();
+        }
+
+    }
+}
