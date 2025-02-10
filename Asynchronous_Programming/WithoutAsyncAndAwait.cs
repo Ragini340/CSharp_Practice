b0bb0feb2049457d5ce6387524342ce9e5eff57e
Ragini340 <ragini31bxr@gmail.com>
2025-02-10 18:58:53 +0530

    #CSharp_Practice Implemented Without Async And Await

---
+++ Asynchronous_Programming/WithoutAsyncAndAwait.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp_Practice.Asynchronous_Programming
+{
+    public class WithoutAsyncAndAwait
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print method started......");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine("\n");
+            Console.WriteLine("Print method end");
+        }
+
+        public static void Main(string[] args)
+        {
+            WithoutAsyncAndAwait withoutAsyncAndAwait = new WithoutAsyncAndAwait();
+            Console.WriteLine("Main method started......");
+            withoutAsyncAndAwait.Print();
+            Console.WriteLine("Main method end");
+        }
+
+    }
+}
