91820c581da511b8ab72bfb3a7f7a418f94d3563
Ragini340 <ragini31bxr@gmail.com>
2025-02-10 19:00:44 +0530

    #CSharp_Practice Implemented Asynchronous Programming Example With Async  and Await

---
+++ Asynchronous_Programming/ExampleWithAsyncAndAwait.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp_Practice.Asynchronous_Programming
+{
+    public class ExampleWithAsyncAndAwait
+    {
+        public async void Print()
+        {
+            Console.WriteLine("Print method started......");
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            Console.WriteLine("\n");
+            Console.WriteLine("Print method end");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main method started......");
+            ExampleWithAsyncAndAwait withAsyncAndAwait = new ExampleWithAsyncAndAwait();
+            withAsyncAndAwait.Print();
+            Console.WriteLine("Main method end");
+            Console.ReadKey(); //Here Console.ReadKey() is necessary to write otherwise it will not print "Print method end"
+                               //statement of Print() on console.
+        }
+
+    }
+}
