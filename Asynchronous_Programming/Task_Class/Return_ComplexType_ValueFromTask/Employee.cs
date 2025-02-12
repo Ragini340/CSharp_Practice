49549e48d29bcf01e90295dbbcf70854dbbb9d1f
Ragini340 <ragini31bxr@gmail.com>
2025-02-12 10:28:56 +0530

    #CSharp_Practice Example to Return Complex Type Value From a Task

---
+++ Asynchronous_Programming/Task_Class/Return_ComplexType_ValueFromTask/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharp_Practice.Asynchronous_Programming.Task_Class.Return_ComplexType_ValueFromTask
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ Asynchronous_Programming/Task_Class/Return_ComplexType_ValueFromTask/ReturnComplexType_FromTask.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharp_Practice.Asynchronous_Programming.Task_Class.Return_ComplexType_ValueFromTask
+{
+    public class ReturnComplexType_FromTask
+    {
+        private async void Print()
+        {
+            Employee emp = await GetEmployeeDetails();
+            Console.WriteLine($"Id: {emp.Id}, Name : {emp.Name}, Salary : {emp.Salary}");
+        }
+
+        public async Task<Employee> GetEmployeeDetails()
+        {
+            Employee employee = new Employee()
+            {
+                Id = 100,
+                Name = "Mike",
+                Salary = 10000
+            };
+
+            return employee;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+            ReturnComplexType_FromTask complexType_FromTask = new ReturnComplexType_FromTask();
+            complexType_FromTask.Print();
+            Console.WriteLine("Main thread completed");
+        }
+
+    }
+}
