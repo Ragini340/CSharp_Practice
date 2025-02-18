8d7859cf67cb51fd56bcf1c8b0e0e28c2285d4fb
Ragini340 <ragini31bxr@gmail.com>
2025-02-18 08:00:49 +0530

    #CSharp_Practice Implemented Limiting the Number of Concurrent Tasks

---
+++ Asynchronous_Programming/Task_Class/LimitTheNumberOfConcurrentTasks/CreditCard.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharp_Practice.Asynchronous_Programming.Task_Class.LimitTheNumberOfConcurrentTasks
+{
+    public class CreditCard
+    {
+        public string CardNumber { get; set; }
+        public string Name { get; set; }
+    }
+}
