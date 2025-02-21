aee81b589241a2797c7bcf3d59ca9af6844a3cfb
Ragini340 <ragini31bxr@gmail.com>
2025-02-21 23:54:21 +0530

    #CSharp_Practice LINQ Where Method Using Separate Function

---
+++ LINQ/Where_Methods/WhereMethod_Ex_Using_SeparateFunction.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharp_Practice.LINQ.Where_Methods
+{
+    public class WhereMethod_Ex_Using_SeparateFunction
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Method syntax
+            IEnumerable<int> filteredData = list.Where(num => CheckNumber(num));
+            foreach (int number in filteredData)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+
+        public static bool CheckNumber(int number)
+        {
+            if (number > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
