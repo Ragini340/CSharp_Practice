51d3abd51ccc23fe5a2064f5352da05e752339d9
Ragini340 <ragini31bxr@gmail.com>
2025-02-21 07:22:50 +0530

    #CSharp_Practice Implemented Where Method Using Method & Query Syntax

---
+++ LINQ/Where_Methods/WhereMethodEx.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharp_Practice.LINQ.Where_Methods
+{
+    public class WhereMethodEx
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //Method Syntax
+            IEnumerable<int> filteredData = list.Where(num => num > 5);
+            Console.WriteLine("Using method syntax:");
+            foreach (int number in filteredData)
+            {
+                Console.Write(number + " ");
+            }
+
+            //Query Syntax
+            IEnumerable<int> filteredResult = from num in list
+                                              where num > 5
+                                              select num;
+            Console.WriteLine("\nUsing query syntax:");
+            foreach (int number in filteredResult)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+    }
+}
