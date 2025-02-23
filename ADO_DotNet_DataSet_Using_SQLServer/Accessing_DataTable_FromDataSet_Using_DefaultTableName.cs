a0808f93c3458b1eb1a9f4f201989022c7f838da
Ragini340 <ragini31bxr@gmail.com>
2025-02-23 20:36:09 +0530

    #CSharp_Practice Accessing Data table from DataSet Using Default Table Name

---
+++ ADO_DotNet_DataSet_Using_SQLServer/Accessing_DataTable_FromDataSet_Using_DefaultTableName.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace CSharp_Practice.ADO_DotNet_DataSet_Using_SQLServer
+{
+    public class Accessing_DataTable_FromDataSet_Using_DefaultTableName
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string ConnectionString = @"data source=DESKTOP-LBBRO23\SQLEXPRESS; database=ShoppingCartDB; TrustServerCertificate=True; Integrated Security=SSPI";
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from customers; select * from orders", connection);
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet);
+
+                    Console.WriteLine("Table 1 data:");
+                    foreach (DataRow row in dataSet.Tables["Table"].Rows)
+                    {
+                        Console.WriteLine(row["Id"] + ",  " + row["Name"] + ",  " + row["Mobile"]);
+                    }
+
+                    Console.WriteLine("\nTable 2 data:");
+                    foreach (DataRow row in dataSet.Tables["Table1"].Rows)
+                    {
+                        Console.WriteLine(row[0] + ",  " + row[1] + ",  " + row[2]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+            }
+        }
+
+    }
+}
