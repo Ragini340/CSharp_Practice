0ccbf0c35bee25c5a3bcc58cb9f163db33ebfc08
Ragini340 <ragini31bxr@gmail.com>
2025-02-25 11:39:34 +0530

    #CSharp_Practice Program to swap two numbers without using a third variable

---
+++ CodingQuestions/SwapTwoNums_WithoutThirdVariable.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class SwapTwoNums_WithoutThirdVariable
+    {
+        public static void SwapTwoNums(int a, int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("After swap num1 is: " + a + "\n" + "After swap num2 is: " + b);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            SwapTwoNums(num1, num2);
+        }
+
+    }
+}
