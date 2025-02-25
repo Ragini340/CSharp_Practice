b30e5a8521ad6c07801b99d3a187b021c4d6f994
Ragini340 <ragini31bxr@gmail.com>
2025-02-25 18:07:04 +0530

    #CSharp_Practice Program to find the count of zeros occurring from 0 till the given number

---
+++ CodingQuestions/CountNumbersHavingZeroAsADigit.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class CountNumbersHavingZeroAsADigit
+    {
+        public static int CountNumbersHavingZeroTillTheGivenNumber(int num)
+        {
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (i.ToString().Contains('0'))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The count of zeros occurring from 0 till the given number is: " + CountNumbersHavingZeroTillTheGivenNumber(num));
+        }
+
+    }
+}
