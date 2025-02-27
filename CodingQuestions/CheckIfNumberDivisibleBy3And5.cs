147c5fd721d17772207ac391b22876677c18039d
Ragini340 <ragini31bxr@gmail.com>
2025-02-27 07:41:23 +0530

    #CSharp_Practice Program to check If number is divisible by 3 and 5

---
+++ CodingQuestions/CheckIfNumberDivisibleBy3And5.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class CheckIfNumberDivisibleBy3And5
+    {
+        public static bool IsNumberDivisibleBy3And5(int num)
+        {
+            if (num % 15 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input number: ");
+            int num = int.Parse(Console.ReadLine());
+            bool result = IsNumberDivisibleBy3And5(num);
+            Console.WriteLine("Is input number is divisible by 3 and 5? " + result);
+        }
+
+    }
+}
