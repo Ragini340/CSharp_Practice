d988e48764361163cd23721a978391247b38b138
Ragini340 <ragini31bxr@gmail.com>
2025-03-02 16:46:44 +0530

    #CSharp_Practice Returning Multiple Values to a Method Caller using the out Keyword

---
+++ Return_Using_Out_Keyword/ReturnUsingOutKeyword.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp_Practice.Return_Using_Out_Keyword
+{
+    public class ReturnUsingOutKeyword
+    {
+        public static void GetValuesUsingOutKeyword(out string stringValue, out bool boolValue, out int intValue)
+        {
+            stringValue = "Ragini";
+            boolValue = false;
+            intValue = int.MaxValue;
+        }
+
+        public static void Main(string[] args)
+        {
+            string stringValue;
+            bool boolValue;
+            int intValue;
+            GetValuesUsingOutKeyword(out stringValue, out boolValue, out intValue);
+            Console.WriteLine(stringValue);
+            Console.WriteLine(boolValue);
+            Console.WriteLine(intValue);
+        }
+
+    }
+}
