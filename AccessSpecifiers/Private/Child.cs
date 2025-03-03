a2fd3b33fd67e5d27229d544f299fb9d9f9a4b06
Ragini340 <ragini31bxr@gmail.com>
2025-03-03 08:03:21 +0530

    #CShrap_Practice Implemented Private Access Modifier

---
+++ AccessSpecifiers/Private/Child.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp_Practice.AccessSpecifiers.Private
+{
+    public class Child : PrivateAccessSpecifiersPOC
+    {
+        public void ShowValue()
+        {
+            //Trying to access data inside a derived class
+            //Error: 'member' is inaccessible due to its protection level
+            //Console.WriteLine("Value = " + data);
+        }
+    }
+}
--- AccessSpecifiers/Private/PrivateAccessSpecifiersPOC.cs
+++ AccessSpecifiers/Private/PrivateAccessSpecifiersPOC.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Practice.AccessSpecifiers.Private
+﻿namespace CSharp_Practice.AccessSpecifiers.Private
 {
+    /*
+     private access modifier limits access to members of a class so that only the class itself can use them. Private members cannot 
+     be accessed from outside the class, not even by derived classes.
+     */
     public class PrivateAccessSpecifiersPOC
     {
         private int data;
+        public void SetValue(int value)
+        {
+            data = value;
+        }
+
+        public int GetValue()
+        {
+            return data;
+        }
+
+        public static void Main(string[] args)
+        {
+            PrivateAccessSpecifiersPOC privateAccessSpecifiers = new PrivateAccessSpecifiersPOC();
+            privateAccessSpecifiers.SetValue(4);
+            Console.WriteLine("Value = " + privateAccessSpecifiers.GetValue());
+        }
+
     }
 }
