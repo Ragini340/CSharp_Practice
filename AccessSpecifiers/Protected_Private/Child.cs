89b2decee09bb4c01a0f7fd2fb0e566b91139786
Ragini340 <ragini31bxr@gmail.com>
2025-03-03 08:33:55 +0530

    #CSharp_Practice Implemented Protected Private Modifier

---
+++ AccessSpecifiers/Protected_Private/Child.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp_Practice.AccessSpecifiers.Protected_Private
+{
+    public class Child : Protected_PrivateModifier_POC
+    {
+        public void ShowValue()
+        {
+            //Access value inside a derived class
+            Console.WriteLine("Value = " + value);
+        }
+
+        public static void Main(string[] args)
+        {
+            Protected_PrivateModifier_POC protectedPrivate = new Protected_PrivateModifier_POC();
+            //Error: Cannot access protected member 'member' via a qualifier of type 'type1'; the qualifier must be of type 'type2'
+            //(or derived from it)
+            //protectedPrivate.value = 5;
+
+            //Using public functions to assign and using value of the member 'value'
+            protectedPrivate.SetValue(4);
+            Console.WriteLine("Value = " + protectedPrivate.GetValue());
+        }
+
+    }
+}
---
+++ AccessSpecifiers/Protected_Private/Protected_PrivateModifier_POC.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp_Practice.AccessSpecifiers.Protected_Private
+{
+    /*
+     protected private access is only granted to the containing class. Any other class inside the current or another assembly is not
+     granted access to these members. 
+     */
+    public class Protected_PrivateModifier_POC
+    {
+        private protected int value;
+        public void SetValue(int v)
+        {
+            value = v;
+        }
+        public int GetValue()
+        {
+            return value;
+        }
+
+    }
+}
