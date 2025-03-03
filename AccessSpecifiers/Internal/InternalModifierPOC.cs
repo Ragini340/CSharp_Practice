040a5883deaefe49d4fd83f5da6c46624748472a
Ragini340 <ragini31bxr@gmail.com>
2025-03-03 08:06:52 +0530

    #CSharp_Practice Implemented Internal Access Modifier

---
+++ AccessSpecifiers/Internal/InternalModifierPOC.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp_Practice.AccessSpecifiers.Internal
+{
+    /*
+     Access is limited to only the current Assembly, that is any class or type declared as internal is accessible anywhere inside 
+     the same namespace. It is the default access modifier in C#. It is useful when you want to restrict access to code that should 
+     only be used within the same project or library.
+    */
+    internal class InternalModifierPOC
+    {
+        int real;
+        int img;
+
+        public void SetData(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("Real = {0}", real);
+            Console.WriteLine("Imaginary = {0}", img);
+        }
+
+        public static void Main(string[] args)
+        {
+            InternalModifierPOC internalModifierPOC = new InternalModifierPOC();
+            internalModifierPOC.SetData(2, 1);
+            internalModifierPOC.DisplayData();
+        }
+
+    }
+}
