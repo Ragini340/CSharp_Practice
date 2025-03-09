d4dd3d2676a9d7cfdff19d8bfa07e73ce0a15e3c
Ragini340 <ragini31bxr@gmail.com>
2025-03-09 08:37:25 +0530

    #CSharp_Practice Program to remove duplicate entries from an array

---
+++ CodingQuestions/Arrays/ToRemoveDuplicateEntriesFromArray.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp_Practice.CodingQuestions.Arrays
+{
+    public class ToRemoveDuplicateEntriesFromArray
+    {
+        public static void RemoveDuplicateEntriesFromArray(int[] arr)
+        {
+            HashSet<int> set = new HashSet<int>(arr);
+            foreach (int num in set)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the array length: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] arr = new int[length];
+            Console.WriteLine("Elements of array are:-");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Array element {0}: ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("After removing duplicate entries array elements are:");
+            RemoveDuplicateEntriesFromArray(arr);
+        }
+
+    }
+}
