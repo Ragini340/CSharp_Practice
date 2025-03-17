6f9f353985f487bd4e6297906612a429e27de7b2
Ragini340 <ragini31bxr@gmail.com>
2025-03-17 21:19:40 +0530

    #CSharp_Practice Program to write a LINQ query to fetch even numbers from a list

---
+++ CodingQuestions/LINQ/QueryToFetchEvenNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp_Practice.CodingQuestions.LINQ
+{
+    //Given a list, write a LINQ query to fetch even numbers.
+    public class QueryToFetchEvenNumbers
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evenNumbers = from number in list
+                              where number % 2 == 0
+                              select number;
+
+            Console.WriteLine("Even numbers are: ");
+            foreach (var number in evenNumbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+    }
+}
