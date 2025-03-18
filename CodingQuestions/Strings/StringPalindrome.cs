352c0321388bf45f1deb8ffd9e9a8186346147f1
Ragini340 <ragini31bxr@gmail.com>
2025-03-18 06:59:22 +0530

    #CSharp_Practice Updated String Palindrome

--- CodingQuestions/Strings/StringPalindrome.cs
+++ CodingQuestions/Strings/StringPalindrome.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Practice.CodingQuestions.Strings
+﻿namespace CSharp_Practice.CodingQuestions.Strings
 {
     public class StringPalindrome
     {
         public static bool IsPalindromeString(string str)
         {
-            str = str.ToLower();
             string reverse = "";
 
             for (int i = str.Length - 1; i >= 0; i--)
