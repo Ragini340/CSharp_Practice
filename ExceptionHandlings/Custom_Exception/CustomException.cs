da06c598671ed37996274903a648808c917192ff
Ragini340 <ragini31bxr@gmail.com>
2025-03-19 21:13:27 +0530

    #CSharp_Practice Implemented Custom Exception

---
+++ ExceptionHandlings/Custom_Exception/CustomException.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp_Practice.ExceptionHandlings.Custom_Exception
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+        }
+
+        public int InvalidAge { get; set; }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int age = -5; 
+                if (age < 0)
+                {
+                    throw new CustomException("Age cannot be negative.") { InvalidAge = age };
+                }
+
+                Console.WriteLine("Age is valid.");
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine($"Invalid age: {ex.InvalidAge}");
+            }
+        }
+
+    }
+}
