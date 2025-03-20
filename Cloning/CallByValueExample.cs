6cee50ac965174e0d98bfa239969899a0fae17e8
Ragini340 <ragini31bxr@gmail.com>
2025-03-20 09:14:40 +0530

    #CSharp_Practice Implemented Call By Value Example

---
+++ Cloning/CallByValueExample.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp_Practice.Cloning
+{
+    public class CallByValueExample
+    {
+        public static void ModifyValue(int num)
+        {
+            Console.WriteLine($"Value of num before modification: {num}");
+            num = num * 2;
+            Console.WriteLine($"Value of num after modification: {num}");
+        }
+
+        public static void Main(string[] args)
+        {
+            int number = 10;
+            Console.WriteLine("Before calling ModifyValue method:");
+            Console.WriteLine($"Value of number: {number}");
+
+            ModifyValue(number);
+            Console.WriteLine("After calling ModifyValue method:");
+            Console.WriteLine($"Value of number: {number}");
+        }
+
+    }
+}
