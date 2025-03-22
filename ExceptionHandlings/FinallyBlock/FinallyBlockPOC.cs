36207276d8e73049786f93a33ee7eaf9f9910882
Ragini340 <ragini31bxr@gmail.com>
2025-03-22 20:44:43 +0530

    #CSharp_Practice Implemented POCs of Finally Block

---
+++ ExceptionHandlings/FinallyBlock/FinallyBlockPOC.cs
@@ -0,0 +1,38 @@
+﻿namespace CSharp_Practice.ExceptionHandlings.FinallyBlock
+{
+    public class FinallyBlockPOC
+    {
+        public static void FinallyBlockUses()
+        {
+            int num1, num2, result;
+            try
+            {
+                Console.WriteLine("Enter the first number:");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second number:");
+                num2 = int.Parse(Console.ReadLine());
+
+                result = num1 / num2;
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Second number should not be zero");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter only integer numbers");
+            }
+            finally
+            {
+                Console.WriteLine("Hello, this is finally block");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            FinallyBlockUses();
+        }
+
+    }
+}
---
+++ ExceptionHandlings/FinallyBlock/Try_FinallyBlockWithoutCatchBlock.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp_Practice.ExceptionHandlings.FinallyBlock
+{
+    public class Try_FinallyBlockWithoutCatchBlock
+    {
+        public static void TestMethod()
+        {
+            try
+            {
+                Console.WriteLine("Inside TestMethod");
+                int num1 = 10, num2 = 0;
+                int result = num1 / num2;
+                Console.WriteLine($"Result: {result}");
+            }
+            finally
+            {
+                Console.WriteLine("TestMethod finally block");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                TestMethod();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception caught");
+            }
+        }
+
+    }
+}
