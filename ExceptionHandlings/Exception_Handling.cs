c01b6c3f5172f1cfd18e192fd7846f622c61a9b2
Ragini340 <ragini31bxr@gmail.com>
2025-03-22 10:33:48 +0530

    #CSharp_Practice Exception Handling POC

---
+++ ExceptionHandlings/Exception_Handling.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp_Practice.ExceptionHandlings
+{
+    public class Exception_Handling
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 1;
+
+            try
+            {
+                int result = b / a;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception occurred" + ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Exception occurred" + ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
