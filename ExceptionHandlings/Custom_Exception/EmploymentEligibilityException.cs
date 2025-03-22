919f09c27429b5a878d27e2ffa2b926000e5f056
Ragini340 <ragini31bxr@gmail.com>
2025-03-22 20:05:28 +0530

    #CSharp_Practice Implemented Custom Exception Example

---
+++ ExceptionHandlings/Custom_Exception/EmploymentEligibilityException.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp_Practice.ExceptionHandlings.Custom_Exception
+{
+    public class EmploymentEligibilityException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Employement age should be greater than equal to 21 and less than 35.";
+            }
+        }
+
+    }
+}
---
+++ ExceptionHandlings/Custom_Exception/EmploymentForm.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharp_Practice.ExceptionHandlings.Custom_Exception
+{
+    public class EmploymentForm
+    {
+        public static void Main(string[] args)
+        {
+            int age;
+            try
+            {
+                Console.WriteLine("Enter the age:");
+                age = int.Parse(Console.ReadLine());
+
+                if (age > 35 || age <= 21)
+                {
+                    throw new EmploymentEligibilityException();
+                }
+                Console.WriteLine("Employment age " + age + " is valid.");
+            }
+            catch (EmploymentEligibilityException ex)  //If I comment this catch block then exception will be thrown from next catch
+                                                      //block i.e. from super class Exception class and it will display same custom
+                                                     //message which I have written.
+                                                      
+            {
+                Console.WriteLine($"Error message: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error message: {ex.Message}");
+            }
+            
+            Console.WriteLine("End of the Program");
+        }
+
+    }
+}
