ca5baf1637ce39f7653d67ede346ad15893be8ff
Ragini340 <ragini31bxr@gmail.com>
2025-03-23 09:59:34 +0530

    #CSharp_Practice Implemented Example of Exception Handling Abuse

---
+++ ExceptionHandlings/ExceptionHandling_Abuse/ExceptionHandlingAbuse.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharp_Practice.ExceptionHandlings.ExceptionHandling_Abuse
+{
+    public class ExceptionHandlingAbuse
+    {
+        public static void ExceptionHandlingsAbuse()
+        {
+            try
+            {
+                Console.WriteLine("Enter the first number: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second number: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                int result = num1 / num2;
+                Console.WriteLine($"Result = {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only numbers are allowed");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Only numbers between {Int32.MinValue} & {Int32.MaxValue} are allowed");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Second number cannot be zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            ExceptionHandlingsAbuse();
+        }
+
+    }
+}
