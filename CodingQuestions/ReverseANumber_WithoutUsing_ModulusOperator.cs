00a1f6a60620431c5ed51336953165dd22c3df0b
Ragini340 <ragini31bxr@gmail.com>
2025-03-25 22:43:27 +0530

    #CSharp_Practice Program to reverse a number without using the modulus operator

---
+++ CodingQuestions/ReverseANumber_WithoutUsing_ModulusOperator.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class ReverseANumber_WithoutUsing_ModulusOperator
+    {
+        public static int ReverseNumber(int number)
+        {
+            string str = number.ToString();
+            char[] numberArr = str.ToCharArray();
+            Array.Reverse(numberArr);
+            string reversedString = new string(numberArr);
+            return int.Parse(reversedString);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int reversedNumber = ReverseNumber(number);
+            Console.WriteLine("Reversed number is: " + reversedNumber);
+        }
+
+    }
+}
