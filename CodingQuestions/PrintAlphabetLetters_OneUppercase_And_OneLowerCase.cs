5ebd607fd156ee1fe6187f3efc820ebdbbf5b9e7
Ragini340 <ragini31bxr@gmail.com>
2025-03-26 08:46:35 +0530

    #CSharp_Practice Program to print alphabet letters, one uppercase and one lower case

---
+++ CodingQuestions/PrintAlphabetLetters_OneUppercase_And_OneLowerCase.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class PrintAlphabetLetters_OneUppercase_And_OneLowerCase
+    {
+        public static void AlphabetLettersOneUppercaseAndOneLowerCase()
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                char letter = (char)('A' + i);
+                if (i % 2 == 0)
+                {
+                    Console.Write(letter + " ");
+                }
+                else
+                {
+                    Console.Write(char.ToLower(letter) + " ");
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            AlphabetLettersOneUppercaseAndOneLowerCase();
+        }
+
+    }
+}
