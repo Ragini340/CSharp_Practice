f3981c64660a84dac143cee11f1ffc4346916b76
Ragini340 <ragini31bxr@gmail.com>
2025-03-26 10:28:56 +0530

    #CSharp_Practice Program to find square root of a number

---
+++ CodingQuestions/SquareRootOfANumber.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharp_Practice.CodingQuestions
+{
+    public class SquareRootOfANumber
+    {
+        public static void FindSquareRootOfANumber(int num)
+        {
+            int squareRoot = (int)Math.Sqrt(num);
+            Console.WriteLine($"The square root of {num} is: {squareRoot}");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+            FindSquareRootOfANumber(num);
+        }
+
+    }
+}
