c4ffbef5072a05788493dd142e6d06e9ffad9754
Ragini340 <ragini31bxr@gmail.com>
2025-03-27 23:35:23 +0530

    #CSharp_Practice Modified Second Largest Element in List

--- CodingQuestions/Lists/SecondLargestElementInList.cs
+++ CodingQuestions/Lists/SecondLargestElementInList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Practice.CodingQuestions.Lists
+﻿namespace CSharp_Practice.CodingQuestions.Lists
 {
     //Program to find second largest number in a list
     /*Time Complexity: O(N + N) = O(2N) = N 
@@ -12,18 +6,8 @@ namespace CSharp_Practice.CodingQuestions.Lists
     */
     public class SecondLargestElementInList
     {
-        public static void Main(string[] args)
+        public static void SecondLargestElementOfList(List<int> lists)
         {
-           List<int> lists = new List<int> { 5, 3, 2, 6, 7, 7, 7 };
-           //Checking for negative scenario with below list:-
-           // List<int> lists = new List<int> { 7, 7, 7 };
-            Console.WriteLine("Elements of list are: ");
-            foreach (int list in lists)
-            {
-                Console.WriteLine(list);
-            }
-            Console.WriteLine();
-
             lists.Sort();
             Console.WriteLine("Sorted list's elements in ascending order are: ");
             for (int i = 0; i < lists.Count; i++)
@@ -42,19 +26,33 @@ namespace CSharp_Practice.CodingQuestions.Lists
             {
                 if (lists[i] != lists[lists.Count - 1])
                 {
-                   secondLargest = lists[i];
-                   flag = true;
-                   break;
+                    secondLargest = lists[i];
+                    flag = true;
+                    break;
                 }
             }
             if (flag)
             {
                 Console.WriteLine("Second largest number is: " + secondLargest);
-            } 
+            }
             else
             {
                 Console.WriteLine("Element not found");
             }
+        }
+
+        public static void Main(string[] args)
+        {
+           List<int> lists = new List<int> { 5, 3, 2, 6, 7, 7, 7 };
+            SecondLargestElementOfList(lists);
+           //Checking for negative scenario with below list:-
+           // List<int> lists = new List<int> { 7, 7, 7 };
+            Console.WriteLine("Elements of list are: ");
+            foreach (int list in lists)
+            {
+                Console.WriteLine(list);
+            }
+            Console.WriteLine();  
         }
 
     }
