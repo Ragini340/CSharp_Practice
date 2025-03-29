ee68ee29af6a3cf1f950ae4cc7b7e873b081a252
Ragini340 <ragini31bxr@gmail.com>
2025-03-29 18:48:17 +0530

    #CShrap_Practice Renamed Class

---
+++ ExceptionHandlings/UnhandledExceptionPOC.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharps_Practice.ExceptionHandlings
+{
+    public class UnhandledExceptionPOC
+    {
+        public static void Main(String[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine(arr[7]);
+        }
+
+    }
+}
