f410cdd315b2c57a8fed7a09b936bace0f53edff
Ragini340 <ragini31bxr@gmail.com>
2025-03-30 12:26:14 +0530

    #CSharp_Practice Updated String Palindrome

--- String_POC/Strings/StringPalindrome.cs
+++ String_POC/Strings/StringPalindrome.cs
@@ -1,43 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Practice.String_POC.Strings
+﻿namespace CSharp_Practice.String_POC.Strings
 {
     /*Time Complexity: O(n), where n is the length of the input string.
-    Auxiliary Space: O(1), no extra data structures used.*/
+      Space Complexity: O(1), no extra data structures used.*/
     public class StringPalindrome
     {
-        //Function to check if a string is a palindrome
-        static int IsPalindrome(string s)
+        public static bool IsPalindrome(string s)
         {
             int left = 0;
             int right = s.Length - 1;
 
-            //Continuing looping while the two pointers have not crossed
             while (left < right)
             {
-                // If the characters at the current position are not equal
                 if (s[left] != s[right])
                 {
-                    return 0;
+                    return false;
                 }
 
-                //Moving the left pointer to the right and the right pointer to the left
                 left++;
                 right--;
             }
 
-            // If no mismatch is found, return 1 (palindrome)
-            return 1;
+            //If no mismatch is found, return true (palindrome)
+            return true;
         }
 
-        static void Main()
+        public static void Main(string[] args)
         {
             string s = "abba";
-            Console.WriteLine(IsPalindrome(s));
+            Console.WriteLine("Is input string is palindrome? " + IsPalindrome(s));
         }
+
     }
 }
