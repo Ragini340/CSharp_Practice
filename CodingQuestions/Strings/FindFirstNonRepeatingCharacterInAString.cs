bc98cab4bdc3351ef09d525612eb7ef4c38a7a02
Ragini340 <ragini31bxr@gmail.com>
2025-03-31 14:33:38 +0530

    #CSharp_Practice Program to find the first non-repeating character in a string

---
+++ CodingQuestions/Strings/FindFirstNonRepeatingCharacterInAString.cs
@@ -0,0 +1,39 @@
+﻿namespace CSharp_Practice.CodingQuestions.Strings
+{
+    public class FindFirstNonRepeatingCharacterInAString
+    {
+        public static void FindFirstNonRepeatingCharacter(string str)
+        {
+            Dictionary<char, int> frequencyDic = new Dictionary<char, int>();
+
+            foreach (char ch in str)
+            {
+                if (frequencyDic.ContainsKey(ch))
+                {
+                    frequencyDic[ch]++;
+                }
+                else
+                {
+                    frequencyDic[ch] = 1;
+                }
+            }
+
+            foreach (char ch in str)
+            {
+                if (frequencyDic[ch] == 1)
+                {
+                    Console.WriteLine("First non-repeating character is: " + ch);
+                    break;
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input string");
+            string str = Console.ReadLine();
+            FindFirstNonRepeatingCharacter(str);
+        }
+
+    }
+}
