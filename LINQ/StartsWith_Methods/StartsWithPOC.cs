1545e96f8fd4c3b60401fb8b335d9f38a23baacb
Ragini340 <ragini31bxr@gmail.com>
2026-04-03 10:00:00 +0530

    #CSharp_Practice Count words starting with letter 'A' using LINQ

---
+++ LINQ/StartsWith_Methods/StartsWithPOC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp_Practice.Threads.MultiThreading.Synchronization.Using_Mutex;
+
+namespace CSharp_Practice.LINQ.StartsWith_Methods
+{
+    /*Count words starting with letter 'A' using LINQ.*/
+    public class StartsWithPOC
+    {
+        public static void CountWords()
+        {
+            List<string> words = new List<string>
+            {
+            "Apple",
+            "Ant",
+            "Ball",
+            "Animal",
+            "Cat"
+            };
+
+            int count = words.Count(w => w.StartsWith("A"));
+
+            Console.WriteLine($"Count: {count}");
+        }
+
+        public static void Main(string[] args)
+        {
+            CountWords();
+        }
+    }
+}
