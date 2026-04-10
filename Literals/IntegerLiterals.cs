733b414aa3353acb4a1316fcd6d71606aa89dfdf
Ragini340 <ragini31bxr@gmail.com>
2026-04-10 23:09:36 +0530

    #CSharp_Practice Added example demonstrating integer literals

---
+++ Literals/IntegerLiterals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Literals
+{
+    public class IntegerLiterals
+    {
+        public static void Main(string[] args)
+        {
+            // Decimal literal
+            //Allowed Digits: 0 to 9	
+            int a = 101; //No suffix is required
+            // Hexa-Decimal Literal
+            //Allowed Digits: 0 to 9 and Character a to f
+            int c = 0x123f; //Prefix with 0x, and suffix with f
+            //Binary literal
+            //Allowed Digits: 0 to 1
+            int d = 0b1111; //Prefix with 0b
+            Console.WriteLine($"Decimal Literal: {a}");
+            Console.WriteLine($"Hexa-Decimal Literal: {c}");
+            Console.WriteLine($"Binary Literal: {d}");
+        }
+    }
+}
