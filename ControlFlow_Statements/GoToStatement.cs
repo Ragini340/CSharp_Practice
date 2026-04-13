bd0d1b4fef3db76a93d3b5c7e3ea24d589731b9f
Ragini340 <ragini31bxr@gmail.com>
2026-04-13 19:13:19 +0530

    #CSharp_Practice Added example demonstrating use of goto statement

---
+++ ControlFlow_Statements/GoToStatement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.ControlFlow_Statements
+{
+    public class GoToStatement
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Executing Statment1");
+            goto statement4;   //goto label someLabel:(here statement4:)
+            Console.WriteLine("Executing Statment2");
+            Console.WriteLine("Executing Statment3");
+            statement4:    //label:
+            Console.WriteLine("Executing Statment4");
+            Console.WriteLine("Executing Statment5");
+        }
+    }
+}
