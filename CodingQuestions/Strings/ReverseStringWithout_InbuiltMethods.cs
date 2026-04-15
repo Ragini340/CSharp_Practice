a9de71af38b019bf579980068f9a33ce501ea3be
Ragini340 <ragini31bxr@gmail.com>
2026-04-15 21:39:37 +0530

    #CSharp_Practice Added string reversal without using inbuilt methods

---
+++ CodingQuestions/Strings/ReverseStringWithout_InbuiltMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.CodingQuestions.Strings
+{
+    public class ReverseStringWithout_InbuiltMethods
+    {
+        public string ReverseString(string input)
+        {
+            char[] chars = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                chars[i] = input[input.Length - 1 - i];
+            }
+
+            string reversed = new string(chars);
+            return reversed;
+        }
+
+        public static void Main(string[] args)
+        {
+            ReverseStringWithout_InbuiltMethods obj = new ReverseStringWithout_InbuiltMethods();
+
+            string input = "hello";
+            string reversed = obj.ReverseString(input);
+
+            Console.WriteLine(reversed);
+        }
+    }
+}
