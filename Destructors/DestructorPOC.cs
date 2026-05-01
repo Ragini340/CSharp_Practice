9ef285bf8a149d2cf29e242a14f462b33e53dc9d
Ragini340 <ragini31bxr@gmail.com>
2026-05-01 09:28:04 +0530

    #CSharp_Practice Destructor POC

---
+++ Destructors/DestructorPOC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Destructors
+{
+    public class DestructorPOC
+    {
+        public DestructorPOC()
+        {
+            Console.WriteLine("Constructor is called");
+        }
+
+        ~DestructorPOC()
+        {
+            string type = GetType().Name;
+            Console.WriteLine($"Object {type} is Destroyed");
+        }
+
+        public static void Main(string[] args)
+        {
+            DestructorPOC destructorPOC = new DestructorPOC();
+            DestructorPOC destructorPOC1 = new DestructorPOC();
+            destructorPOC = null;
+            destructorPOC1 = null;
+            GC.Collect();
+        }
+    }
+}
---
+++ Destructors/First.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Destructors
+{
+    public class First
+    {
+        ~First()
+        {
+            Console.WriteLine("Destructor of First Class");
+        }
+    }
+}
