132265ce0474e8d75618fe058fe70ea585da4de0
ARCHANA KUMARI <ms.archanacusat2015@gmail.com>
2026-05-01 10:25:19 +0530

    #CSharp_Practice Destructor POC

---
+++ Destructors/Third.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Destructors
+{
+    public class Third : Second
+    {
+        ~Third  ()
+        {
+            Console.WriteLine("Destructor of Third Class");
+        }
+
+        public static void Main(string[] args)
+        {
+            Third obj = new Third();
+            obj = null;
+            GC.Collect();
+        }
+    }
+}
