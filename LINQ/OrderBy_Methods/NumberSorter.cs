8a81c97c720589974583050d86d3fd14aad7c274
Ragini340 <ragini31bxr@gmail.com>
2026-05-28 11:58:21 +0530

    #CSharp_Practice Implemented number sorting using LINQ method

---
+++ LINQ/OrderBy_Methods/NumberSorter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.LINQ.OrderBy_Methods
+{
+    public class NumberSorter
+    {
+        public static void SortNumbers(int[] nums)
+        {
+            var sorted = nums.OrderBy(x => x);
+
+            Console.WriteLine("Sorted Numbers:");
+
+            foreach (var num in sorted)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = { 5, 2, 8, 1, 4 };
+
+            SortNumbers(nums);
+        }
+    }
+}
