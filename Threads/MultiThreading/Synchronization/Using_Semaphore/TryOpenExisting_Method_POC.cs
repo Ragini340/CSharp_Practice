948f27e5bde20b982f7881d8a1162a9f59e86e95
Ragini340 <ragini31bxr@gmail.com>
2026-06-02 09:06:37 +0530

    #CSharp_Practice TryOpenExisting Method POC of Semaphore class

---
+++ Threads/MultiThreading/Synchronization/Using_Semaphore/TryOpenExisting_Method_POC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Using_Semaphore
+{
+    public class TryOpenExisting_Method_POC
+    {
+        public static void Main(string[] args)
+        {
+            Semaphore sem;
+
+            if (Semaphore.TryOpenExisting("SemaphoreDemoObject", out sem))
+            {
+                Console.WriteLine("Semaphore exists.");
+                Console.WriteLine("Another instance is running.");
+            }
+            else
+            {
+                sem = new Semaphore(1, 1, "NewSemaphoreDemoObject");
+                Console.WriteLine("New semaphore created.");
+            }
+        }
+    }
+}
