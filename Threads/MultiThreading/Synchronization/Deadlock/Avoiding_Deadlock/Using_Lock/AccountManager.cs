225783539d4e55952fe4585ca15c4bb2d73c299c
Ragini340 <ragini31bxr@gmail.com>
2026-06-05 08:52:52 +0530

    #CSharp_Practice Updated Accountanager to Avoid Deadlock by acquiring locks in a specific order

--- Threads/MultiThreading/Synchronization/Deadlock/Avoiding_Deadlock/Using_Lock/AccountManager.cs
+++ Threads/MultiThreading/Synchronization/Deadlock/Avoiding_Deadlock/Using_Lock/AccountManager.cs
@@ -7,6 +7,7 @@ namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Deadlock.Avoidi
         private Account FromAccount;
         private Account ToAccount;
         private readonly double TransferAmount;
+        private static readonly Mutex mutex = new Mutex();
 
         public AccountManager(Account AccountFrom, Account AccountTo, double AmountTransfer)
         {
@@ -17,21 +18,33 @@ namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Deadlock.Avoidi
 
         public void FundTransfer()
         {
-            object _lock1 = FromAccount;
-            object _lock2 = ToAccount;
+            object _lock1, _lock2;
+            if (FromAccount.Id < ToAccount.Id)
+            {
+                _lock1 = FromAccount;
+                _lock2 = ToAccount;
+            }
+            else
+            {
+                _lock1 = ToAccount;
+                _lock2 = FromAccount;
+            }
 
             Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {((Account)_lock1).Id}");
-            lock (FromAccount)
+            lock (_lock1)
             {
                 Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {((Account)_lock1).Id}");
                 Console.WriteLine($"{Thread.CurrentThread.Name} doing some work");
+                Thread.Sleep(3000);
                 Console.WriteLine($"{Thread.CurrentThread.Name} trying to acquire lock on {((Account)_lock2).Id}");
-                lock (ToAccount)
+                lock (_lock2)
                 {
                     Console.WriteLine($"{Thread.CurrentThread.Name} acquired lock on {((Account)_lock2).Id}");
                     FromAccount.WithdrawMoney(TransferAmount);
                     ToAccount.DepositMoney(TransferAmount);
                 }
+                Console.WriteLine($"{Thread.CurrentThread.Name} released lock on {((Account)_lock2).Id}"); 
+                Console.WriteLine($"{Thread.CurrentThread.Name} released lock on {((Account)_lock1).Id}");
             }
         }
 
