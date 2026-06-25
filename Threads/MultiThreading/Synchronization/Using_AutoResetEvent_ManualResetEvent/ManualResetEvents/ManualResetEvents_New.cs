31c1e7ce1bd085f4c30a99398577cfb2aa07f027
Ragini340 <ragini31bxr@gmail.com>
2026-06-25 15:40:54 +0530

    #Practice Updated ManualResetEvents

--- Threads/MultiThreading/Synchronization/Using_AutoResetEvent_ManualResetEvent/ManualResetEvents/ManualResetEvents_New.cs
+++ Threads/MultiThreading/Synchronization/Using_AutoResetEvent_ManualResetEvent/ManualResetEvents/ManualResetEvents_New.cs
@@ -1,6 +1,12 @@
-﻿namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Using_AutoResetEvent_ManualResetEvent.ManualResetEvents
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice.Threads.MultiThreading.Synchronization.Using_AutoResetEvent_ManualResetEvent.ManualResetEvents
 {
-    public class ManualResetEventPOC
+    public class ManualResetEvents_New
     {
         ManualResetEvent manualResetEvent = new ManualResetEvent(false);
 
@@ -11,8 +17,14 @@
             {
                 Console.WriteLine(Thread.CurrentThread.Name + " " + i);
             }
-            Console.WriteLine("Set() has been called from Thread1");
+            manualResetEvent.WaitOne();
             manualResetEvent.Set();
+
+            for (int i = 41; i < 51; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+            }
+            Console.WriteLine("Set() has been called from Thread1");
         }
 
         public void Thread2Method()
@@ -24,20 +36,22 @@
             }
             Console.WriteLine("WaitOne() has been called on Thread2");
             manualResetEvent.WaitOne();
+
             for (int i = 11; i < 21; i++)
             {
                 Console.WriteLine(Thread.CurrentThread.Name + " " + i);
-            } 
+            }
+            manualResetEvent.Set();
         }
 
         public static void Main(string[] args)
         {
-            ManualResetEventPOC manualReset = new ManualResetEventPOC();
-            Thread thread1 = new Thread(manualReset.Thread1Method)
+            ManualResetEvents_New autoReset = new ManualResetEvents_New();
+            Thread thread1 = new Thread(autoReset.Thread1Method)
             {
                 Name = "Thread1"
             };
-            Thread thread2 = new Thread(manualReset.Thread2Method)
+            Thread thread2 = new Thread(autoReset.Thread2Method)
             {
                 Name = "Thread2"
             };
